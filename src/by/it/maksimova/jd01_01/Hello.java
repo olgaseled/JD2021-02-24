3e8f5dc05ef867c61ab2e07dfc6b37099b2c2a35
maksimova <ps_nat98@mail.ru>
2021-03-05 11:48:19 +0300

    jd01-04

---
+++ src/by/it/maksimova/jd01_01/Hello.java
@@ -0,0 +1,13 @@
+package by.it.maksimova.jd01_01;
+
+public class Hello {
+    private String slogan="Hello world";
+
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;
+    }
+
+    void pringSlogan(){
+        System.out.println(slogan);;
+    }
+}
