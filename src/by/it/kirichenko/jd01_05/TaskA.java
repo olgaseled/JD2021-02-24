c2db2221a0e3eb00b906336cdb5a56b2c39a66eb
Roman Pupko <romanpupko1994@gmail.com>
2021-03-05 12:34:48 +0300

    jd05.4 student

--- src/by/it/kirichenko/jd01_05/TaskA.java
+++ src/by/it/kirichenko/jd01_05/TaskA.java
@@ -1,18 +1,19 @@
 package by.it.kirichenko.jd01_05;
 
+import static java.lang.Math.*;
+
+//import static java.lang.Math.*;
 public class TaskA {
     public static void main(String[] args) {
         step1();
         step2();
         step3();
-
-
     }
 
     private static void step1() {
         double x=0.3;
         double a=756.13;
-        double part1=Math.cos(Math.pow(x*x+Math.PI/6,5));
+        double part1=cos(pow(x*x+PI/6,5));
         double part2=Math.sqrt(x*Math.pow(a,3));
         double part3=Math.log(a-1.12*x/4);
         double z=part1-part2-part3;
