a1147ee3589504f35e50202772ff2a5195881f90
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-05 11:48:49 +0300

    jd01-02

---
+++ src/by/it/levchuk/levchuk/jd01_01/Main.java
@@ -0,0 +1,16 @@
+package by.it.levchuk.levchuk.jd01_01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Hello hello=new Hello();
+        hello.printslogan();
+        hello.setSlogan("Привет мир");
+        hello.printslogan();
+
+
+        Args argObject=new Args(args);
+        argObject.printArgs();
+        // write your code here
+    }
+}
