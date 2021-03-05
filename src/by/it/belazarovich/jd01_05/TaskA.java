0857ef312a4fb41fd4673f5a57cad3c4d0da03ae
Andrey <and.rey882012@gmail.com>
2021-03-05 12:17:58 +0300

    JD01-04 A

---
+++ src/by/it/belazarovich/jd01_05/TaskA.java
@@ -0,0 +1,25 @@
+package by.it.belazarovich.jd01_05;
+
+import static java.lang.Math.cos;
+import static java.lang.StrictMath.pow;
+
+public class TaskA {
+    public static void main(String[] args) {
+        
+        step1();
+        Object step2;
+        step2;
+        
+    }
+    private static void step1(){
+        
+    private static final Object PI = ;
+
+    {
+        double partl = cos(pow([*+PI/6, 5]));
+        double x = 0.3;
+        double a = 756.13;
+        double part1 = cos
+        }
+    }
+}
