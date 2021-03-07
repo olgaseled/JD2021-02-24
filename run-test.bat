ff6cc260f7ad183391cd3a5bbfc0fa45d2efc163
Aleksei Dudko <don't@mail.me>
2021-03-08 00:14:36 +0300

    refactor: group and move up main script parameters

--- run-test.bat
+++ run-test.bat
@@ -1,11 +1,12 @@
+SET CLASS_NAME=TaskB
+SET LAB_NUM=jd01_05
+SET TEST_NAME=Test_jd01_05
+
 SET SRC_PATH=c:\java-lab\JD2021-02-24\src
 SET OUTPUT_PATH=c:\java-lab\JD2021-02-24\classes
 SET USER_PATH=by\it\dudko
 SET USER_PACKAGE=by.it.dudko
 SET JUNIT_PACKAGE=org.junit.runner.JUnitCore
-SET LAB_NUM=jd01_05
-SET CLASS_NAME=TaskA
-SET TEST_NAME=Test_jd01_05
 
 javac -d %OUTPUT_PATH% %SRC_PATH%\%USER_PATH%\%LAB_NUM%\%CLASS_NAME%.java
 javac -d %OUTPUT_PATH% -cp .;lib.jar %SRC_PATH%\%USER_PATH%\%LAB_NUM%\%TEST_NAME%.java
