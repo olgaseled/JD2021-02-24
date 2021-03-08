558d657072f8bdc808f68163a95b912eacf6a080
Илья <ilya.papruga@gmail.com>
2021-03-09 00:31:05 +0300

    jd01_04 TaskB complete

--- src/by/it/papruga/jd01_04/TaskB.java
+++ src/by/it/papruga/jd01_04/TaskB.java
@@ -11,11 +11,6 @@ public class TaskB {
         int n = scanner.nextInt();
         String[] lastName = new String[n];
         int sumSalary = 0;
-        int quarter1 = 0;
-        int quarter2 = 0;
-        int quarter3 = 0;
-        int quarter4 = 0;
-
         for (int i = 0; i < n; i++) {
             lastName[i] = scanner.next();
         }
@@ -25,20 +20,14 @@ public class TaskB {
         for (int i = 0; i < n; i++) {
 
             System.out.println("Введите зарплату для " + lastName[i]);
-
             for (int j = 0; j < 4; j++) {
                 salary[i][j] = scanner.nextInt();
             }
         }
 
-        for (int i = 0; i < n; i++) { sumSalary = sumSalary + Arrays.stream(salary[i]).sum(); }
-
-        for (int i = 0; i < n; i++) { quarter1 = quarter1 + salary[i][0]; } quarter1 = quarter1/n;
-        for (int i = 0; i < n; i++) { quarter2 = quarter2 + salary[i][1]; } quarter2 = quarter2/n;
-        for (int i = 0; i < n; i++) { quarter3 = quarter3 + salary[i][2]; } quarter3 = quarter3/n;
-        for (int i = 0; i < n; i++) { quarter4 = quarter4 + salary[i][3]; } quarter4 = quarter4/n;
-        double averageSalary = (double) (quarter1 + quarter2 + quarter3 + quarter4) / 4;
-
+        for (int i = 0; i < n; i++) {
+            sumSalary = sumSalary + Arrays.stream(salary[i]).sum();
+        }
         System.out.println("-".repeat(60));
         System.out.println("Фамилия  Квартал1  Квартал2   Квартал3   Квартал4   Итого");
         System.out.println("-".repeat(60));
@@ -47,7 +36,8 @@ public class TaskB {
         }
         System.out.println("-".repeat(60));
         System.out.println("Итого " + sumSalary);
-        System.out.printf("Средняя %.4f", averageSalary);
+        //  System.out.printf("Средняя %.4f", averageSalary);
+        System.out.printf("Средняя %.4f", ((float) sumSalary / 4) / 3);
 
     }
 
