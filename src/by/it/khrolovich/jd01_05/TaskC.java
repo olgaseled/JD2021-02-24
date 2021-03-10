f5c8910f46f725e1b2ce29457fee187545bebb62
MarinaKhrolovich <maniuneshka@gmail.com>
2021-03-10 11:13:01 +0300

    commit finished

--- src/by/it/khrolovich/jd01_05/TaskC.java
+++ src/by/it/khrolovich/jd01_05/TaskC.java
@@ -2,6 +2,10 @@ package by.it.khrolovich.jd01_05;
 
 public class TaskC {
     public static void main(String[] args) {
+        step1();
+    }
 
+    private static void step1() {
+        int n = 20+(int)Math.random()*19;
     }
 }
