71fe57059d07ac35a57d39af8ec249f526d8c85c
Korotkevich Alexey <lyoshakorotkevich0909@gmail.com>
2021-03-11 14:24:47 +0300

    jd01_01 - jd01_06

--- src/by/it/korotkevich/jd01_06/TaskA2.java
+++ src/by/it/korotkevich/jd01_06/TaskA2.java
@@ -12,28 +12,28 @@ public class TaskA2 {
 
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
-        StringBuilder text = new StringBuilder(Poem.text);
-        Matcher matcher = pattern.matcher(text);
+        Matcher matcher = pattern.matcher(Poem.text);
         while (matcher.find()) {
             String word = matcher.group();
             processOneWord(word);
         }
         for (int i = 0; i < words.length; i++) {
-            System.out.printf("%s=%d", words[i], counts[i]);
+            System.out.printf("%s=%d\n", words[i], counts[i]);
         }
     }
 
     private static void processOneWord(String word) {
-        for (int i = 0; i < words.length; i++) {
+        for (int i = 0, wordsLength = words.length; i < wordsLength; i++) {
             String currentWord = words[i];
             if (currentWord.equals(word)) {
+                counts[i] = counts[i] + 1;
                 return;
             }
         }
-        final int newLength = words.length+1;
-        words = Arrays.copyOf(words,newLength);
-        counts = Arrays.copyOf(counts,newLength);
-        words[words.length-1]=word;
-        counts[counts.length-1]=1;
+        final int newLength = words.length + 1;
+        words = Arrays.copyOf(words, newLength);
+        counts = Arrays.copyOf(counts, newLength);
+        words[words.length - 1] = word;
+        counts[counts.length - 1] = 1;
     }
 }
--- src/by/it/korotkevich/jd01_06/TaskB1.java
+++ src/by/it/korotkevich/jd01_06/TaskB1.java
@@ -2,14 +2,24 @@ package by.it.korotkevich.jd01_06;
 
 public class TaskB1 {
 
-    private static final String votes= "уеэоаыяиюёУЕЭОАЫЯИЮЁ";
+    private static final String votes= "уеэоаыяиюёУЕЫАОЭЯИЮЁ";
 
     public static void main(String[] args) {
-        boolean result=checkOneWord("Вася");
+        String[] arrayOfWords=Poem.text.split("\\s+");
+        for (int i = 0; i < arrayOfWords.length-1; i++) {
+            arrayOfWords[i] = arrayOfWords[i].replaceAll("[^а-яА-ЯёЁ]", "");
+        }
+
+        for (int word = 0; word < arrayOfWords.length-1; word++) {
+            boolean result = checkOneWord(arrayOfWords[word]);
+            if (result) {
+                System.out.println(arrayOfWords[word]);
+            }
+        }
     }
 
     private static boolean checkOneWord(String word) {
-
+        if (word.length()==0) return false;
         return votes.indexOf(word.charAt(0))<0
                 && votes.indexOf(word.charAt(word.length()-1))>=0;
     }
