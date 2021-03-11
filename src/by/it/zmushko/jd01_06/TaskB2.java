06528f1ed3bd3197063b4b846a998ac464cd418c
Vitalya <zmuskovitaliy@gmail.com>
2021-03-11 22:41:04 +0300

    jd01-06  некрасивое B

---
+++ src/by/it/zmushko/jd01_06/TaskB2.java
@@ -0,0 +1,46 @@
+package by.it.zmushko.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB2 {
+
+    private static String[] mySplitText = {};
+
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder(Poem.text);
+        findThreeDot(text);
+        mySplitText = text.toString().split("[.!?]");
+
+        for (int i = 0; i < mySplitText.length; i++) {
+            mySplitText[i] = mySplitText[i].replaceAll("[^а-яА-ЯёЁ]", " ").replaceAll(" {2,}", " ").trim();
+        }
+
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < mySplitText.length - 1; i++) {
+                String temp;
+
+                if (mySplitText[i].length() > mySplitText[i + 1].length()) {
+                    temp = mySplitText[i];
+                    mySplitText[i] = mySplitText[i + 1];
+                    mySplitText[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+        for (String s : mySplitText) {
+            System.out.println(s);
+        }
+    }
+
+    public static void findThreeDot(StringBuilder text) {
+        Pattern pattern = Pattern.compile("\\.{3}");
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            text.replace(matcher.start(), matcher.end(), "");
+        }
+    }
+
+}
