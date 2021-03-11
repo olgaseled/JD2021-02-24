4f7f0fbff8ae413e206aafaa283a7bbeb6e8f301
maksimova <ps_nat98@mail.ru>
2021-03-11 17:13:19 +0300

    jd01-04

--- src/by/it/maksimova/jd01_06/TaskA2.java
+++ src/by/it/maksimova/jd01_06/TaskA2.java
@@ -6,34 +6,40 @@ import java.util.regex.Pattern;
 
 
 public class TaskA2 {
-    private static String [] words = {};
-    private static int [] counts = {};
+
+    private static String[] words = {};
+    private static int[] counts = {};
+
+    private static int pos(String word) {
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word))
+                return i;
+        }
+            return -1;
+        }
+
+
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
         Matcher matcher = pattern.matcher(Poem.text);
-        while (matcher.find()){
+        while (matcher.find()) {
             String word = matcher.group();
-            processOneWord(word);
+            int p = pos(word);
+            if (p >= 0) {
+                counts[p]++;
+            } else {
+                int last = words.length;
+                words = Arrays.copyOf(words, last + 1);
+                words[last] = word;
+                counts = Arrays.copyOf(counts, last + 1);
+                counts[last] = 1;
+            }
         }
         for (int i = 0; i < words.length; i++) {
             System.out.printf("%s=%d", words[i], counts[i]);
             System.out.println();
         }
     }
-
-    private static void processOneWord(String word) {
-        for (String currentWord : words) {
-            if (currentWord.equals(word)) {
-//                counts[i]=counts[i]+1;
-                return;
-            }
-
-        }
-        words= Arrays.copyOf(words,words.length+1);
-        counts= Arrays.copyOf(counts,words.length+1);
-        words[words.length-1]=word;
-        counts[counts.length-1]=1;
-
-    }
 }
+
 
