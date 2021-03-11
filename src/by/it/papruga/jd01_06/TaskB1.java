5144b40a31745d1d6cc5919ea2f3848ee2e929c4
Илья <ilya.papruga@gmail.com>
2021-03-12 01:07:44 +0300

    jd01_06 A1 A2 B1

--- src/by/it/papruga/jd01_06/TaskB1.java
+++ src/by/it/papruga/jd01_06/TaskB1.java
@@ -1,11 +1,23 @@
 package by.it.papruga.jd01_06;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskB1 {
 
     private static final String votes = "уеэоаыяиюёУЕЫАОЭЯИЮЁ";
 
     public static void main(String[] args) {
+
         boolean result = chekOneWord("Вася");
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find ()) {
+
+            if (chekOneWord(matcher.group()) == result) {
+                System.out.println(matcher.group());
+            }
+        }
     }
 
     private static boolean chekOneWord(String word) {
