11b35f0598e4944108f68e1e8669533f70329882
maksimova <ps_nat98@mail.ru>
2021-03-11 16:00:19 +0300

    jd01-04

--- src/by/it/maksimova/jd01_06/TaskA2.java
+++ src/by/it/maksimova/jd01_06/TaskA2.java
@@ -4,12 +4,36 @@ import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class TaskA2 {
+    private static String [] words = {};
+    private static int [] counts = {};
     public static void main(String[] args) {
-//TaskA2.Определить, сколько раз повторяется в тексте каждое слово, которое встречается в нем, используя массивы.
-//       Вывести результаты на консоль в формате слово=повторы
-
-
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()){
+            String word = matcher.group();
+            processOneWord(word);
+        }
+        for (int i = 0; i < words.length; i++) {
+            System.out.printf("%s=%d", words[i], counts[i]);
+            System.out.println();
+        }
     }
 
+    private static void processOneWord(String word) {
+        for (String currentWord : words) {
+            if (currentWord.equals(word)) {
+//                counts[i]=counts[i]+1;
+                return;
+            }
+
+        }
+        words= Arrays.copyOf(words,words.length+1);
+        counts= Arrays.copyOf(counts,words.length+1);
+        words[words.length-1]=word;
+        counts[counts.length-1]=1;
+
+    }
 }
+
