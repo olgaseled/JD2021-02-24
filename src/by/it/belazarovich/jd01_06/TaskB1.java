44a3c8fc18be4d60536c31f061329889767f9c47
Andrey <and.rey882012@gmail.com>
2021-03-11 22:13:16 +0300

    add B1

--- src/by/it/belazarovich/jd01_06/TaskB1.java
+++ src/by/it/belazarovich/jd01_06/TaskB1.java
@@ -1,5 +1,25 @@
 package by.it.belazarovich.jd01_06;
 
-public class TaskB1 {
+import java.util.Arrays;
 
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        String s;
+        s = Poem.text;
+
+        String letters = "БВГДЖЗЙКЛМНПРСТФХЦЧШЩбвгджзйклмнпрстфхцчшщ";
+        Arrays.stream(s.split("\\s+"))
+                .filter(x -> letters.contains(x.substring(0,1)))
+
+                .forEach(System.out::println);
+    }
 }
+/*
+  //      String letters = "БВГДЖЗЙКЛМНПРСТФХЦЧШЩбвгджзйклмнпрстфхцчшщ";
+        Pattern pattern = Pattern.compile("БВГДЖЗЙКЛМНПРСТФХЦЧШЩбвгджзйклмнпрстфхцчшщ");
+        for (String s : wordArray) {
+            Matcher matcher = pattern.matcher(word);
+            if (matcher.find()) {
+                System.out.println(word);
+ */
