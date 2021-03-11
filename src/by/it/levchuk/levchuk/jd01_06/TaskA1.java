3b996107fe5a4f10d6be352a74a827ca0b1024ae
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-11 22:55:51 +0300

    jd01-06 Homework TaskA

--- src/by/it/levchuk/levchuk/jd01_06/TaskA1.java
+++ src/by/it/levchuk/levchuk/jd01_06/TaskA1.java
@@ -7,12 +7,12 @@ public class TaskA1 {
     public static void main(String[] args) {
         StringBuilder sbd = new StringBuilder(Poem.text);
         Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]{4,}");
-        Matcher matcher= pattern.matcher(Poem.text);
-        while (matcher.find()){
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
             int start = matcher.start();
-            sbd.setCharAt(start+3,'#');
-            if (matcher.group().length()>=7){
-                sbd.setCharAt(start+6,'#');
+            sbd.setCharAt(start + 3, '#');
+            if (matcher.group().length() >= 7) {
+                sbd.setCharAt(start + 6, '#');
             }
         }
         System.out.println(sbd);
