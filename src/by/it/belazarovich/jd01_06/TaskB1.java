a4356d994629815cba22f84c20d0683eb9a054e4
Andrey <and.rey882012@gmail.com>
2021-03-11 12:20:25 +0300

    JD01 2021.03.10

---
+++ src/by/it/belazarovich/jd01_06/TaskB1.java
@@ -0,0 +1,19 @@
+package by.it.belazarovich.jd01_06;
+/*
+public class TaskB1 {
+    public static void main(String[] args) {
+        boolean result = checkOneWord ("Вася");
+
+
+    }
+    privat static boolean checkOneWord (String word){
+        char first = word.charAt(0);
+        char last = word.charAt(word.length()-1);
+
+        return wotes.indexOf(word.charAt(word.length()-1))>=0;
+
+    }
+}
+
+
+ */
