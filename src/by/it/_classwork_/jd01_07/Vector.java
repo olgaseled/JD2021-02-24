2cc687a8bb77090be54ae8dc78e638a8c48c272f
Alexander Khmelov <akhmelev@gmail.com>
2021-03-12 22:00:26 +0300

    classwork 01_07

--- src/by/it/_classwork_/jd01_07/Vector.java
+++ src/by/it/_classwork_/jd01_07/Vector.java
@@ -14,13 +14,17 @@ public class Vector extends Var {
     @Override
     public String toString() {
         // {1.0, 3.5, 5.8}
-//        StringBuilder strVector = new StringBuilder("{");
-////        String delimiter = "";
-////        for (int v : value) {
-////            strVector.append(delimiter).append(v);
-////            delimiter = ", ";
-////        }
-////        strVector.append("}");
+
+        /*
+        StringBuilder strVector = new StringBuilder("{");
+        String delimiter = "";
+        for (int v : value) {
+            strVector.append(delimiter).append(v);
+            delimiter = ", ";
+        }
+        strVector.append("}");
+        */
+
         StringJoiner stringJoiner = new StringJoiner(", ", "{", "}");
         for (Double v : value) {
             stringJoiner.add(v.toString());
