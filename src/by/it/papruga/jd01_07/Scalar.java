49776d257c94ff2b054a36013dd067f542a75355
Oleg Zh <ffieryiceadd@gmail.com>
2021-03-12 12:27:03 +0300

    jd01_07

---
+++ src/by/it/papruga/jd01_07/Scalar.java
@@ -0,0 +1,26 @@
+package by.it.papruga.jd01_07;
+
+public class Scalar extends Var {
+
+    private final double value;
+
+
+    public Scalar(double value) {
+        this.value = value;
+    }
+
+    public Scalar(String strValue) {
+        this.value = Double.parseDouble(strValue);
+    }
+
+    public Scalar(Scalar otherScalar) {
+        this.value = otherScalar.value;
+    }
+
+    @Override
+    public String toString() {
+        return "Scalar{" +
+                "value=" + value +
+                '}';
+    }
+}
