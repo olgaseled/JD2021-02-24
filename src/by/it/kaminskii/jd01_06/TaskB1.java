de2dbb0adac93d3370065aa1cc79acb8065aa7b0
vanek323 <ivankaminskii323@gmail.com>
2021-03-12 11:15:44 +0300

    jd01_05

---
+++ src/by/it/kaminskii/jd01_06/TaskB1.java
@@ -0,0 +1,43 @@
+//package by.it.kaminskii.jd01_06;
+//
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//
+//public class TaskB1 {
+//    private static String[] vovels = new String[]{"А", "Е", "И", "О", "У", "Э", "Я", "Ю", "Ё", "Ы", "а", "е",
+//            "и", "о", "у", "э", "я", "ю", "ё", "ы"};
+//    private static String[] cons = new String[]{"б", "в", "г", "д", "ж", "з", "й", "к", "л", "м", "н", "п", "р",
+//            "с", "т", "ф", "х", "ц", "ч", "ш", "щ"};
+//
+////
+////    private static int pos(String word) {
+////        String[] split = Poem.text.split(Poem.text);
+////
+////        for (int i = 0; i < split.length; i++) {
+////            if (split[i].equals()) {
+////                System.out.println(split[i]);
+////            }
+////        }
+////    }
+//                public static void main (String[]args){
+//                    Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+//                    Matcher matcher = pattern.matcher(Poem.text);
+//                    while (matcher.find()) {
+//                        int first = matcher.start(1);
+//
+//                        for (int i = 0; i < cons.length; i++) {
+//                           // if(matcher.start(1)==cons[i]){
+//
+//                        }
+//
+//
+//
+//                        }
+//
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
