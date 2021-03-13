41517631680310e49940479abe800b44ad1a30f4
seledtsova <olgaseledtsova9@yandex.by>
2021-03-13 11:59:40 +0300

    jd01-01 jd01-02 jd01-03 jd01-04 jd01-05 jd01-06 jd01-07

--- src/by/it/seledtsova/jd_06/TaskB2.java
+++ src/by/it/seledtsova/jd_06/TaskB2.java
@@ -3,7 +3,6 @@ package by.it.seledtsova.jd_06;
 public class TaskB2 {
     public static void main(String[] args) {
 
-
         String text = Poem.text;
         // 1. delete ...
         text = text.replace("...", "");
@@ -12,25 +11,33 @@ public class TaskB2 {
         //3. del non-letters;
         for (int i = 0; i < sentences.length; i++) {
             sentences[i] = sentences[i].replaceAll("[^а-яА-ЯёЁ]", " ").trim();
-          //   System.out.println(sentences[i]);
-
+            //   System.out.println(sentences[i]);
         }
-
-        for (int i = 0; i < sentences.length; i++) {
-            for (int j = 0; j < sentences.length; j++) {
-                if (sentences[i].compareTo(sentences[j]) < 0) {
-                    String temp = sentences[i];
+        // 4.
+        for (int i = 0; i <= sentences.length - 1; i++) {
+            for (int j = i + 1; j <= sentences.length - 1; j++) {
+                if (sentences[j].compareTo(sentences[i]) < 0) {
+                    String buffer = sentences[i];
                     sentences[i] = sentences[j];
-                    sentences[j] = temp;
-                    System.out.println(sentences[j]);
+                    sentences[j] = buffer;
                 }
-            }
-        }
-
-
 
             }
+            System.out.println(sentences[i]);
         }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
