8579eefa689f07e37b0f7b1f6eecbf0db59220ca
baranov <alvadiv@inbox.ru>
2021-03-13 15:48:26 +0100

    jd01-04 a b

--- src/by/it/baranov/jd01_04/TaskB.java
+++ src/by/it/baranov/jd01_04/TaskB.java
@@ -5,75 +5,42 @@ import java.util.Scanner;
 public class TaskB {
 
 
-    private static Object pbsv;
+
 
     public static void main(String[] args) {
-            // Объявим коснтанту для размера массива
 
 
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        String[] name = new String[s];
+        for (int i = 0; i < name.length; i++) { name[i] = sc.next();
 
-
-            Scanner scanner = new Scanner(System.in);
-
-            String[] lastNames;
-            int v;
-
-
-            Scanner in = new Scanner(System.in);
-            v= in.nextInt();
-            int m = 0;
-            lastNames=new String[v];
-            for (int i = 0; i < lastNames.length; i++) {lastNames[i] = scanner.nextLine();}
-            // Цикл по второй размерности (вторые квадратные скобки)
+        }
 
 
 
            // System.out.println(Arrays.deepToString(lastNames));
             // Создаем двумерный массив
-            String[][] graph;
-            graph = new String[3][4];
+            Integer[][] graph;
+            graph = new Integer[s][4];
 
 
 
 
 
-
-            System.out.print("Vvedite zarplatu");
-            System.out.print(" ");
-            System.out.println(lastNames[0]);
 
 
             // Цикл по первой размерности (первые квадратные скобки)
-            for (int i = 0; i < graph.length; i++) {
+            for (int i = 0; i < graph.length; i++) {System.out.print("Vvedite zarplatu");
+                System.out.print(" "); System.out.print(name[i]); System.out.println();
                 // Цикл по второй размерности (вторые квадратные скобки)
-                for (int j = 0; j < graph[i].length; j++) {
+                for (int j = 0; j < graph[0].length; j++)
 
-                    graph[i][j] = String.valueOf(scanner.nextInt());}
-                break;}
+                    graph[i][j] = sc.nextInt();}
 
-            System.out.print("Vvedite zarplatu");
-            System.out.print(" ");
-            System.out.println(lastNames[1]);
 
-            // Цикл по первой размерности (первые квадратные скобки)
-            for (int i = 1; i < graph.length; i++) {
-                // Цикл по второй размерности (вторые квадратные скобки)
-                for (int j = 0; j < graph[i].length; j++) {
+                    ;
 
-                    graph[i][j] = String.valueOf(scanner.nextInt());}
-                break;}
-
-            System.out.print("Vvedite zarplatu");
-            System.out.print(" ");
-            System.out.println(lastNames[2]);
-
-            // Цикл по первой размерности (первые квадратные скобки)
-            for (int i = 2; i < graph.length; i++) {
-                // Цикл по второй размерности (вторые квадратные скобки)
-                for (int j = 0; j < graph[i].length; j++) {
-
-                    graph[i][j] = String.valueOf(scanner.nextInt());}
-                break;}
             //System.out.println(" Vvedite zarplatu Petrov");
             //graph[i][j] = String.valueOf(scanner.nextInt());}}
 
@@ -89,19 +56,30 @@ public class TaskB {
 
 
             System.out.println();
-            System.out.format("%-12s%-12s%-12s%-12s%-12s\n", lastNames[0], graph[0][0], graph[0][1], graph[0][2], graph[0][3]);
-            System.out.format("%-12s%-12s%-12s%-12s%-12s\n",lastNames[1],graph[1][0],graph[1][1],graph[1][2],graph[1][3]);
-            System.out.format("%-12s%-12s%-12s%-12s%-12s\n",lastNames[2],graph[2][0],graph[2][1],graph[2][2],graph[2][3]);
+            System.out.format("%-12s%-12s%-12s%-12s%-12s\n", name[0], graph[0][0], graph[0][1], graph[0][2], graph[0][3]);
+            System.out.format("%-12s%-12s%-12s%-12s%-12s\n",name[1],graph[1][0],graph[1][1],graph[1][2],graph[1][3]);
+            System.out.format("%-12s%-12s%-12s%-12s%-12s\n",name[2],graph[2][0],graph[2][1],graph[2][2],graph[2][3]);
 
            int sum;
+           int res;
+           sum=0;
+           res=0;
 
-           // for (int i = 0; i < graph.length;) { sum=0;
-                // Цикл по второй размерности (вторые квадратные скобки)
-               // for (int j = 0; j < graph[i].length; j++) { sum =sum+ graph[0][j];
 
-                  //  System.out.println("sum " + sum);
 
-            }}
+        for (int i = 0; i < graph.length; i++) {
+             sum = 0;
+            //System.out.format("%7s: ", name[i]);
+            for (int j = 0; j < graph[0].length; j++) {
+                System.out.println( graph[i][j]);
+                sum += graph[i][j];
+               res += graph[i][j];;}
+
+                System.out.println("sum"+sum);
+            System.out.println("Total"+ res);}}}
+
+
+
 
 
 
