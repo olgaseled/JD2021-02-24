3c620053029c2bb571a75f3e8d596dd6036ef84e
Andrey <and.rey882012@gmail.com>
2021-03-13 12:02:10 +0300

    upd T2, T3, T6

--- src/by/it/belazarovich/jd01_06/TaskB1.java
+++ src/by/it/belazarovich/jd01_06/TaskB1.java
@@ -1,9 +1,48 @@
 package by.it.belazarovich.jd01_06;
 
-import java.util.Arrays;
 
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskB1 {
+
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("(?U)\\b[бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ]\\w+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
+            String a = matcher.group();
+            StringBuilder b = new StringBuilder(a);
+            b.reverse();
+
+            Pattern pattern1 = Pattern.compile("(?U)\\b[ауоыиэяюёе]\\w+");
+            Matcher matcher1 = pattern1.matcher(b);
+            while (matcher1.find()) {
+                String c = matcher1.group();
+                StringBuilder d = new StringBuilder(c);
+                d.reverse();
+                System.out.println(d);
+            }
+        }
+    }
+
+    /**
+     * @param word
+     * @return
+     */
+    private static boolean checkOneWord (String word){
+        String votes = "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ";
+        char first = word.toLowerCase().charAt(0);
+        char last = word.toLowerCase().charAt(word.length() - 1);
+        boolean firstOk = votes.indexOf(first) < 0;
+        boolean lastOk = votes.indexOf(last) >= 0;
+        return firstOk && lastOk;
+
+    }
+}
+ /*
     public static void main(String[] args) {
         String s;
         s = Poem.text;
@@ -23,3 +62,4 @@ public class TaskB1 {
             if (matcher.find()) {
                 System.out.println(word);
  */
+
