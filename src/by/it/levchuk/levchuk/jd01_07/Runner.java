65a343c2d7130d55b0c78e3a7e384a111577765a
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-13 12:49:44 +0300

    jd01-07 TaskA1-3

--- src/by/it/levchuk/levchuk/jd01_07/Runner.java
+++ src/by/it/levchuk/levchuk/jd01_07/Runner.java
@@ -5,9 +5,9 @@ public class Runner {
         Var var1 = new Scalar(3.1415);
         Scalar var2 = new Scalar("3.1415");
         Var var3 = new Scalar(var2);
+
         System.out.printf("var1=%s\n", var1);
         System.out.printf("var2=%s\n", var2);
         System.out.printf("var3=%s\n", var3);
-
     }
 }
