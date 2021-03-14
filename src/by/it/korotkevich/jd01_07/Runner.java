f5da1761272744eb7be8a2c1c852e2ca0aa62ffe
Korotkevich Alexey <lyoshakorotkevich0909@gmail.com>
2021-03-14 16:06:44 +0300

    jd01_07 решены задачи B

--- src/by/it/korotkevich/jd01_07/Runner.java
+++ src/by/it/korotkevich/jd01_07/Runner.java
@@ -9,7 +9,11 @@ public class Runner {
         System.out.printf("var2=%s\n", var2);
         System.out.printf("var3=%s\n", var3);
 
-        Var vec4 = new Vector(new double[]{1, 2, 3, 4, 5});
+        Vector vec4 = new Vector(new double[]{1, 2, 3, 4, 5});
         System.out.printf("vec4=%s\n", vec4);
+        Vector vec5 = new Vector(vec4);
+        System.out.printf("vec5=%s\n", vec5);
+        Vector vec6 = new Vector("{ 3, 5.3, 6.3, 56");
+        System.out.printf("vec6=%s\n", vec6);
     }
 }
--- src/by/it/korotkevich/jd01_07/Vector.java
+++ src/by/it/korotkevich/jd01_07/Vector.java
@@ -6,8 +6,19 @@ public class Vector extends Var {
 
     private final double[] value;
 
-    public Vector(double[] value) {
+    Vector(double[] value) {
         this.value = value;
+    }
+
+    Vector(Vector otherVector) {this.value = otherVector.value; }
+
+    Vector(String strValue) {
+        String[] trimmedString = strValue.replaceAll("[{} ]+", "").trim().split(",");
+        double[] result = new double[trimmedString.length];
+        for (int i = 0; i < trimmedString.length; i++) {
+            result[i]=Double.parseDouble(trimmedString[i]);
+        }
+        this.value = result;
     }
 
     @Override
