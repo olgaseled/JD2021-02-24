f49883df2db0630768631acfead000bf5d7a6ee2
maksimova <ps_nat98@mail.ru>
2021-03-14 21:46:01 +0300

    01-06

--- src/by/it/maksimova/jd01_06/TaskB2.java
+++ src/by/it/maksimova/jd01_06/TaskB2.java
@@ -1,31 +1,31 @@
 package by.it.maksimova.jd01_06;
 
 import java.util.Arrays;
+
 //Вывести через \n все предложения текста в порядке возрастания количества символов (!) в каждом из них.
 // В предложениях нужно сначала заменить все небуквенные символы и их последовательности на один пробели
 // выполнить trim() для каждого предложения.
 public class TaskB2 {
     public static void main(String[] args) {
-       //1 delete ...
+        //1 delete ...
         String poema = Poem.text;
         poema = poema.replace("...", "");
-       // 2 split for sentences
-        String [] sentences = poema.split("[.!?]");
+        // 2 split for sentences
+        String[] sentences = poema.split("[.!?]");
         // 3 replace symbols for spaces
         for (int i = 0; i < sentences.length; i++) {
-            sentences[i] = sentences[i].replaceAll("[^а-яА-ЯёЁ]", " ");
-            sentences[i] = sentences[i].trim();
+            sentences[i] = sentences[i].replaceAll("[^а-яА-ЯёЁ]+", " ").trim();
         }
-        sort (sentences);
-        // 4 sort sentences from small to big
+        sort(sentences);
 
     }
-
-    private static void sort( String[] sentences) {
+    // 4 sort sentences from small to big
+    private static void sort(String[] sentences) {
         boolean isSorted;
         do {
             isSorted = false;
-            for (int i = 0; i < sentences.length - 1; i++) {
+            int last = sentences.length - 1;
+            for (int i = 0; i < last; i++) {
                 if (sentences[i].length() > sentences[i + 1].length()) {
                     String buffer = sentences[i];
                     sentences[i] = sentences[i + 1];
@@ -40,8 +40,8 @@ public class TaskB2 {
     }
 
     private static void printArray(String[] sentences) {
-        for (String sentence : sentences) {
-            System.out.printf("%s\n", sentence);
+        for (int i = 0; i < sentences.length; i++) {
+            System.out.printf("%s\n", sentences[i]);
         }
     }
 }
