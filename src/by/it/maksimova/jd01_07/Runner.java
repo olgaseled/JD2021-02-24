085d8d3cfcd774a462182baf704cced17688b127
maksimova <ps_nat98@mail.ru>
2021-03-14 22:09:26 +0300

    jd01-07

--- src/by/it/maksimova/jd01_07/Runner.java
+++ src/by/it/maksimova/jd01_07/Runner.java
@@ -2,11 +2,16 @@ package by.it.maksimova.jd01_07;
 
 public class Runner {
     public static void main(String[] args) {
-        Var var1 = new Scalar(9);
-        Scalar var2 = new Scalar("12.1234");
+        Var var1 = new Scalar(3.1415);
+        Scalar var2 = new Scalar("3.1415");
         Var var3 = new Scalar(var2);
         System.out.printf("var1=%s\n", var1);
         System.out.printf("var2=%s\n", var2);
         System.out.printf("var3=%s\n", var3);
+
+        Var var4 = new Vector(new double [] {1.0, 2.0, 3.0});
+        System.out.printf("var4=%s\n", var4);
+        Var var5 = new Vector("{1.0, 2.0, 3.0}");
+        System.out.printf("var5=%s\n", var5);
     }
 }
