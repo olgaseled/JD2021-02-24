2e912159ff0f729315cd69c7b262e256bda077f8
baranov <alvadiv@inbox.ru>
2021-03-14 14:49:04 +0100

    jd01-04 a b

--- src/by/it/baranov/jd01_06/TaskA2.java
+++ src/by/it/baranov/jd01_06/TaskA2.java
@@ -5,41 +5,43 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskA2 {
-    public static  String[] w={};
-    public static  int[] count={};
 
+    private static String[] w = {};
+    private static int[] count = {0};
 
-    public static int pos (String word){
-        for (int i=0; i<w.length;i++) {
-            if (w[i].equals (word));
-        return i;
+    private static int pos(String word) {
+        for (int i = 0; i < w.length; i++) {
+            if (w[i].equals(word))
+                return i;
         }
         return -1;
+
     }
 
     public static void main(String[] args) {
-        Pattern pattern=Pattern.compile("[a-яА-ЯёЁ]+");
-        Matcher matcher= pattern.matcher(Poem.text);
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
         while (matcher.find()) {
             String word = matcher.group();
             int p = pos(word);
             if (p >= 0) {
                 count[p]++;
+
             } else {
                 int last = w.length;
-                w = Arrays.copyOf(w, 1 + last);
+                w = Arrays.copyOf(w, last + 1);
                 w[last] = word;
-                count = Arrays.copyOf(count, last+1);
+                count = Arrays.copyOf(count, last + 1);
                 count[last] = 1;
             }
         }
-        for (int i=0;i<w.length;i++) {
+        for (int i = 0; i < w.length; i++) {
             System.out.println(w[i] + "=" + count[i]);
         }
 
-            }
+    }}
 
-    }
+
 
 
 
