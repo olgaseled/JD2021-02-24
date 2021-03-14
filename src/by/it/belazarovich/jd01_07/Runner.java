3c4ac6834e875a00382c4fd7165d20e1884be1bf
Andrey <and.rey882012@gmail.com>
2021-03-14 12:28:15 +0300

    upd T7

--- src/by/it/belazarovich/jd01_07/Runner.java
+++ src/by/it/belazarovich/jd01_07/Runner.java
@@ -1,6 +1,6 @@
 package by.it.belazarovich.jd01_07;
 
-public class Runner {
+public class  Runner {
     public static void main(String[] args) {
         AbstractVar v1 = new Scalar(3.14);
         AbstractVar v2 = new Vector();
