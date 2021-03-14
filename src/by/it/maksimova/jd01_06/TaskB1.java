8f760a6274f09208471357d7351723c2123426fc
maksimova <ps_nat98@mail.ru>
2021-03-14 13:36:24 +0300

    1

--- src/by/it/maksimova/jd01_06/TaskB1.java
+++ src/by/it/maksimova/jd01_06/TaskB1.java
@@ -1,22 +1,24 @@
 package by.it.maksimova.jd01_06;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // TaskB1.Вывести в консоль все слова текста, начинающихся согласной и заканчивающихся гласной буквой
-// (нужно сделать для проверки этого условия приватный метод без regex).
-
-
 public class TaskB1 {
 
     private static final String votes="уеэоаыяиюёУЕЫАОЭЯИЮЁ"; //гласные
 
     public static void main(String[] args) {
-        System.out.println(check("Cлово"));
-        System.out.println(check("ещё"));
-        System.out.println(check("итак"));
-        System.out.println(check("конец"));
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()){
+            String word = matcher.group();
+            if (checkWord(word))
+                System.out.println(word);
+        }
     }
 
-    private static boolean check(String word) { //какое-то слово
+    private static boolean checkWord (String word) { //какое-то слово
         String fistLetter = word.substring(0, 1); //первая буква
         String lastLetter = word.substring(word.length() - 1); //последняя буква
         boolean firstOk = !votes.contains(fistLetter); //проверим, что первой нет в гласных
