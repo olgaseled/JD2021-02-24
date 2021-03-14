ad937d743f43732168e82331d97af96f9cd598cf
maksimova <ps_nat98@mail.ru>
2021-03-14 13:24:04 +0300

    1

---
+++ src/by/it/maksimova/jd01_07/Runner.java
@@ -0,0 +1,12 @@
+package by.it.maksimova.jd01_07;
+
+public class Runner {
+    public static void main(String[] args) {
+        Var var1 = new Scalar(9);
+        Scalar var2 = new Scalar("12.1234");
+        Var var3 = new Scalar(var2);
+        System.out.printf("var1=%s\n", var1);
+        System.out.printf("var2=%s\n", var2);
+        System.out.printf("var3=%s\n", var3);
+    }
+}
---
+++ src/by/it/maksimova/jd01_07/Vector.java
@@ -0,0 +1,14 @@
+package by.it.maksimova.jd01_07;
+
+public class Vector extends Var {
+    private final int [] value;
+
+    public Vector(int[] value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
