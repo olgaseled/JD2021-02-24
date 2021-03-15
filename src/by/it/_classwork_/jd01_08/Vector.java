cc4e7775383998d16192329f806d3534c4a023ee
it.academy.vs@gmail.com <it.academy.vs@gmail.com>
2021-03-15 12:32:16 +0300

    classwork jd01_08

--- src/by/it/_classwork_/jd01_08/Vector.java
+++ src/by/it/_classwork_/jd01_08/Vector.java
@@ -15,22 +15,35 @@ public class Vector extends Var {
         this(vector.value);
     }
 
+    public Vector(String strVector) {
+        //1.0,2.343,987.0
+        String[] strArray = strVector.trim()
+                .replaceAll("\\s+", "")
+                .replace("{", "")
+                .replace("}", "")
+                .split(",");
+        value = new double[strArray.length];
+        for (int i = 0; i < value.length; i++) {
+            value[i] = Double.parseDouble(strArray[i]);
+        }
+    }
+
     @Override
     public Var add(Var other) {
-        if (other instanceof  Scalar){
+        if (other instanceof Scalar) {
             double secondScalar = ((Scalar) other).getValue();
             double[] resultVector = Arrays.copyOf(value, value.length);
             for (int i = 0; i < resultVector.length; i++) {
-                resultVector[i]+=secondScalar;
+                resultVector[i] += secondScalar;
             }
             return new Vector(resultVector);
         }
 
-        if (other instanceof  Vector){
+        if (other instanceof Vector) {
             double[] secondVector = ((Vector) other).value;
             double[] resultVector = Arrays.copyOf(value, value.length);
             for (int i = 0; i < resultVector.length; i++) {
-                resultVector[i]+=secondVector[i];
+                resultVector[i] += secondVector[i];
             }
             return new Vector(resultVector);
         }
