e4710de98a30d944f5c49912cd9b8a8b23f5cc3a
Andrey <and.rey882012@gmail.com>
2021-03-16 00:17:12 +0300

    15.03.2021 00:16

--- src/by/it/belazarovich/jd01_08/Vector.java
+++ src/by/it/belazarovich/jd01_08/Vector.java
@@ -1,5 +1,6 @@
 package by.it.belazarovich.jd01_08;
 
+import java.util.Arrays;
 import java.util.StringJoiner;
 
 class Vector extends Var {
@@ -7,6 +8,27 @@ class Vector extends Var {
 
     Vector(double[] value) {
         this.value = value;
+    }
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+            double[] res = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < res.length; i++) {
+                res[i] = res[i] + ((Scalar) other).getValue()  ;
+            }
+            return new Vector(res);
+
+        }
+        else if (other instanceof Vector){
+            double[] res = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < res.length; i++) {
+                res[i] = res[i] + ((Vector) other).value[i];
+            }
+            return new Vector(res);
+        }
+else
+    return super.add(other);
     }
 
     @Override
