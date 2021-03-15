5370df4db34143cfbc437a43afca1149d5192b80
seledtsova <olgaseledtsova9@yandex.by>
2021-03-15 20:58:15 +0300

    jd01-01 jd01-02 jd01-03 jd01-04 jd01-05 jd01-06 jd01-07 jd01-08

---
+++ src/by/it/seledtsova/jd01_08/Vector.java
@@ -0,0 +1,44 @@
+package by.it.seledtsova.jd01_08;
+
+public class Vector extends Var {
+
+    private final double[] value;
+
+    Vector(double[] value) { // 1 конструктор на вход принимает некоторый массив
+        this.value = value;
+    }
+
+    Vector(Vector stringVector) {
+        this.value = stringVector.value;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{"); // создали стринг билдер и обьявили, что первый разделитель у нас пустой
+        String delimiter = ""; //  первый разделитель будет пустой
+        for (double element:value) {          // переберем все элементы в массиве  value
+            sb.append(delimiter).append(element); // будем печатать сразу разделитель , а за ним элемент
+            delimiter = ", ";               // после первого числа разделитель заменим на запятую  и пробел
+        }                                   // в итоге все след элементу будут размедять запятой с пробелом
+        sb.append("}");
+        return sb.toString();
+    }
+
+    public Vector(String stringValue) {
+        String[] arrayString = stringValue.replaceAll("[{} ]+", "").trim().split(",");
+        double[] result = new double[arrayString.length];
+        for (int i = 0; i < arrayString.length; i++) {
+            result[i]=Double.parseDouble(arrayString[i]);
+        }
+        this.value = result;
+
+    }
+}
+
+
+
+
+
+
+
