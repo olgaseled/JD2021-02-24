6a1d4ca9a3eed8223f087296099db2d81afcf872
vanek323 <ivankaminskii323@gmail.com>
2021-03-15 12:25:59 +0300

    jd01_05

---
+++ src/by/it/kaminskii/jd01_08/Var.java
@@ -0,0 +1,13 @@
+package by.it.kaminskii.jd01_08;
+
+
+abstract class Var{
+    @Override
+    public String toString() {
+        return "3.1415";
+    }
+}
+
+
+//разработайте для класса Var наследника Vector с тремя конструкторами:
+
