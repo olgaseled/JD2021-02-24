da376b9b3b9368ffc9f148f2a66e3d1ee1539a68
seledtsova <olgaseledtsova9@yandex.by>
2021-03-16 17:22:49 +0300

    jd01-01 jd01-02 jd01-03 jd01-04 jd01-05 jd01-06 jd01-07 C1 jd01-08 уровень B (1-4))

--- src/by/it/seledtsova/jd01_07/Matrix.java
+++ src/by/it/seledtsova/jd01_07/Matrix.java
@@ -6,15 +6,21 @@ public class  Matrix extends Var {
 
     private final double[][] arrayValues;
 
-    Matrix(double[][] value) { // 1 конструктор на вход принимает некоторый двумерный массив
-        this.arrayValues = value;
+    public Matrix(double[][] value) {
+        this.arrayValues = new double[value.length][0];
+        for (int i = 0; i < value.length; i++) {
+            this.arrayValues[i] = Arrays.copyOf(value[i], value[i].length);
+        }
+    }
+
+    public Matrix(String strValue) {
+        this(new double[][]{{1, 2}, {3, 4}}); //stub
     }
 
     @Override
     public String toString() {
-        return "Matrix{" +
-                "arrayValues=" + Arrays.toString(arrayValues) +
-                '}';
+        return Arrays.deepToString(arrayValues).replace("[", "{").replace("]", "}");
     }
 }
+
 
--- src/by/it/seledtsova/jd01_08/Matrix.java
+++ src/by/it/seledtsova/jd01_08/Matrix.java
@@ -4,17 +4,23 @@ import java.util.Arrays;
 
 public class  Matrix extends Var {
 
-    private final double[][] arrayValues;
 
-    Matrix(double[][] value) { // 1 конструктор на вход принимает некоторый двумерный массив
-        this.arrayValues = value;
-    }
+        private final double[][] arrayValues;
 
-    @Override
-    public String toString() {
-        return "Matrix{" +
-                "arrayValues=" + Arrays.toString(arrayValues) +
-                '}';
+        public Matrix(double[][] value) {
+            this.arrayValues = new double[value.length][0];
+            for (int i = 0; i < value.length; i++) {
+                this.arrayValues[i] = Arrays.copyOf(value[i], value[i].length);
+            }
+        }
+
+        public Matrix(String strValue) {
+            this(new double[][]{{1, 2}, {3, 4}}); //stub
+        }
+
+        @Override
+        public String toString() {
+            return Arrays.deepToString(arrayValues).replace("[", "{").replace("]", "}");
+        }
     }
-}
 
