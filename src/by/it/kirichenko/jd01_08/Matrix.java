17df4dd1844ab268904d3e6d6bf8410db2fc3890
Kirichenko Konstantin <tinik@mail.ru>
2021-03-16 11:23:44 +0300

    01_08 home task B2

--- src/by/it/kirichenko/jd01_08/Matrix.java
+++ src/by/it/kirichenko/jd01_08/Matrix.java
@@ -1,16 +1,37 @@
 package by.it.kirichenko.jd01_08;
 
+import java.util.Arrays;
 import java.util.StringJoiner;
 
 public class Matrix extends Var {
     private final double[][] value;
 
     public Matrix(double[][] value) {
-        this.value = value;
+        this.value = new double[value.length][0];
+        for (int i = 0; i < value.length; i++) {
+            this.value[i] = Arrays.copyOf(value[i], value[i].length);
+        }
     }
 
     public Matrix(Matrix otherMatrix) {
         this.value = otherMatrix.value;
+    }
+
+    Matrix(String doubleStringArray) {
+        String stringWithoutSpace = doubleStringArray.replaceAll(" ", "");
+        String[] tempArrayString = stringWithoutSpace.split("},\\{");
+        String[] tempArrayAfterSplit = new String[tempArrayString.length];
+        for (int i = 0; i < tempArrayString.length; i++) {
+            tempArrayAfterSplit[i] = tempArrayAfterSplit[i] + tempArrayString[i].replaceAll("[{}]", "").replaceAll(",\"", " ").split(" ");
+        }
+        int countElementsInTempArray = 0;
+        double[][] resultDoubleArray = new double[tempArrayString.length][tempArrayString.length];
+        for (int i = 0; i < resultDoubleArray.length; i++) {
+            for (int j = 0; j < resultDoubleArray.length; j++) {
+                resultDoubleArray[i][j] = Double.parseDouble(tempArrayAfterSplit[countElementsInTempArray++]);
+            }
+        }
+        value = resultDoubleArray;
     }
 
     @Override
--- src/by/it/kirichenko/jd01_08/Vector.java
+++ src/by/it/kirichenko/jd01_08/Vector.java
@@ -1,5 +1,6 @@
 package by.it.kirichenko.jd01_08;
 
+import java.util.Arrays;
 import java.util.StringJoiner;
 
 public class Vector extends Var {
@@ -26,21 +27,79 @@ public class Vector extends Var {
         }
         this.value=arrayDouble;
     }
+	
+	@Override
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+            double secondScalar = ((Scalar) other).getValue();
+            double[] resultVector = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < resultVector.length; i++) {
+                resultVector[i] += secondScalar;
+            }
+            return new Vector(resultVector);
+        }
+
+        if (other instanceof Vector) {
+            double[] secondVector = ((Vector) other).value;
+            double[] resultVector = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < resultVector.length; i++) {
+                resultVector[i] += secondVector[i];
+            }
+            return new Vector(resultVector);
+        }
+        return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar) {
+            double secondScalar = ((Scalar) other).getValue();
+            double[] resultVector = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < resultVector.length; i++) {
+                resultVector[i] -= secondScalar;
+            }
+            return new Vector(resultVector);
+        }
+
+        if (other instanceof Vector) {
+            double[] secondVector = ((Vector) other).value;
+            double[] resultVector = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < resultVector.length; i++) {
+                resultVector[i] -= secondVector[i];
+            }
+            return new Vector(resultVector);
+        }
+        return super.add(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar) {
+            double secondScalar = ((Scalar) other).getValue();
+            double[] resultVector = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < resultVector.length; i++) {
+                resultVector[i] *= secondScalar;
+            }
+            return new Vector(resultVector);
+        }
+
+        if (other instanceof Vector) {
+            double[] secondVector = ((Vector) other).value;
+            double[] resultVector = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < resultVector.length; i++) {
+                resultVector[i] *= secondVector[i];
+            }
+            double result=0;
+            for (double rV : resultVector) {
+                result+=rV;
+            }
+            return new Scalar(result);
+        }
+        return super.add(other);
+    }
 
     @Override
     public String toString() {
-        // {1.0, 3.5, 5.8}
-
-        /*
-        StringBuilder strVector = new StringBuilder("{");
-        String delimiter = "";
-        for (int v : value) {
-            strVector.append(delimiter).append(v);
-            delimiter = ", ";
-        }
-        strVector.append("}");
-        */
-
         StringJoiner stringJoiner = new StringJoiner(", ", "{", "}");
         for (Double v : value) {
             stringJoiner.add(v.toString());
