df965185ad78c3d6684b0f9405ce76a240144e90
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-16 19:38:08 +0300

    jd01-07 TaskB Full

--- src/by/it/levchuk/levchuk/jd01_07/Vector.java
+++ src/by/it/levchuk/levchuk/jd01_07/Vector.java
@@ -15,13 +15,6 @@ public class Vector extends Var {
     }
 
     Vector(String strVector) {
-        //String changeString = strVector.replaceAll("[{}]", "").replaceAll(" ", "");
-        //String[] tempStringArray = changeString.split(",");
-        //double[] tempDoubleArray = new double[tempStringArray.length];
-        //for (int i = 0; i < tempStringArray.length; i++) {
-        //    tempDoubleArray[i] = Double.parseDouble(tempStringArray[i]);
-        //}
-        //values = tempDoubleArray;
         String[] strArray = strVector.trim()
                 .replaceAll("\\s+", "")
                 .replace("{", "")
