e476205e81464a64087d343d45ba12c60e11a4f1
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-16 18:53:39 +0300

    jd01-07 TaskB

--- src/by/it/levchuk/levchuk/jd01_07/Vector.java
+++ src/by/it/levchuk/levchuk/jd01_07/Vector.java
@@ -1,28 +1,40 @@
 package by.it.levchuk.levchuk.jd01_07;
 
 import java.util.Arrays;
-import java.util.StringJoiner;
 
-class Vector extends Var {
+public class Vector extends Var {
 
-    private final double[] value;
+    private final double[] values;
 
-
-    Vector(double[] value) {
-        this.value = value;
-    }
-    Vector(Vector vector){
-        this.value= vector.value;
+    Vector(double[] values) {
+        this.values = values;
     }
 
+    Vector(Vector strVector) {
+        this.values = strVector.values;
+    }
 
+    Vector(String strVector) {
+        //String changeString = strVector.replaceAll("[{}]", "").replaceAll(" ", "");
+        //String[] tempStringArray = changeString.split(",");
+        //double[] tempDoubleArray = new double[tempStringArray.length];
+        //for (int i = 0; i < tempStringArray.length; i++) {
+        //    tempDoubleArray[i] = Double.parseDouble(tempStringArray[i]);
+        //}
+        //values = tempDoubleArray;
+        String[] strArray = strVector.trim()
+                .replaceAll("\\s+", "")
+                .replace("{", "")
+                .replace("}", "")
+                .split(",");
+        values = new double[strArray.length];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = Double.parseDouble(strArray[i]);
+        }
+    }
 
     @Override
     public String toString() {
-        StringJoiner stringJoiner = new StringJoiner(", ", "{", "}");
-        for (Double v : value) {
-            stringJoiner.add(v.toString());
-        }
-        return stringJoiner.toString();
+        return Arrays.toString(values).replace("[", "{").replace("]", "}");
     }
 }
