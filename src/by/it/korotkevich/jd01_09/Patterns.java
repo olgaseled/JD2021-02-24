f860bbd3bde65657ebe5eac7cdcd82254b51f13c
katsia-ryna <kati.18@mail.ru>
2021-03-17 12:30:27 +0300

    jd01_08

---
+++ src/by/it/korotkevich/jd01_09/Patterns.java
@@ -0,0 +1,11 @@
+package by.it.korotkevich.jd01_09;
+
+interface Patterns {
+
+    String OPERATION = "[-+*/]";
+    String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
+    String VECTOR = "\\{" + SCALAR + "(," + SCALAR + ")*}";
+    String MATRIX = "\\{" + VECTOR + "(," + VECTOR + ")*}";
+
+
+}
