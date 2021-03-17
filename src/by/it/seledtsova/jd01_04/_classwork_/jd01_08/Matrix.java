7773028679ef5392fc9c925936d3218b5d13b5a1
seledtsova <olgaseledtsova9@yandex.by>
2021-03-17 11:13:54 +0300

    jd01-01 jd01-02 jd01-03 jd01-04 jd01-05 jd01-06 jd01-07 C1 jd01-08

---
+++ src/by/it/seledtsova/jd01_04/_classwork_/jd01_08/Matrix.java
@@ -0,0 +1,23 @@
+package by.it.seledtsova.jd01_04._classwork_.jd01_08;
+
+import java.util.Arrays;
+
+public class Matrix extends Var {
+    private final double[][] value;
+
+    public Matrix(double[][] value) {
+        this.value = new double[value.length][0];
+        for (int i = 0; i < value.length; i++) {
+            this.value[i] = Arrays.copyOf(value[i], value[i].length);
+        }
+    }
+
+    public Matrix(String strValue) {
+        this(new double[][]{{1, 2}, {3, 4}}); //stub
+    }
+
+    @Override
+    public String toString() {
+        return "Matrix";
+    }
+}
