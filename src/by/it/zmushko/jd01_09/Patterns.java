69cf5ed9935dbe6411541278949b754809205b18
Vitalya <zmuskovitaliy@gmail.com>
2021-03-17 18:59:10 +0300

    jd01-09 full, исправленный MATRIX

--- src/by/it/zmushko/jd01_09/Patterns.java
+++ src/by/it/zmushko/jd01_09/Patterns.java
@@ -5,7 +5,7 @@ interface Patterns {
     String OPERATION = "[-+*/]";
     String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
     String VECTOR = "\\{" + SCALAR + "(," + SCALAR + ")*}";
-    String MATRIX = "{" + VECTOR + VECTOR + "}";
+    String MATRIX = "\\{" + VECTOR + "(," + VECTOR + ")*}";
     /*
     1+111
     2+2.0
