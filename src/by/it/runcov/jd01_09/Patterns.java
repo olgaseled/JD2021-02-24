4feae454d4816d5d813406f047fcff83e7a9a2bb
TimofeyRuncov <timofey.runcov@mail.ru>
2021-03-17 20:26:14 +0300

    jd01_09

--- src/by/it/runcov/jd01_09/Patterns.java
+++ src/by/it/runcov/jd01_09/Patterns.java
@@ -3,6 +3,6 @@ package by.it.runcov.jd01_09;
 interface Patterns {
 
     String OPERATION = "[-+*/]";
-    String SCALAR = "-?[0-9]+(\\.[0-9]+?";
-    String VECTOR = "\\{"+SCALAR+"(,"+SCALAR+"}";
+    String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
+    String VECTOR = "\\{" + SCALAR + "(," + SCALAR + ")*}";
 }
