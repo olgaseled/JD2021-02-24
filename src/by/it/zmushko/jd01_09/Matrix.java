da5b00ae50e76cbd1228f7a4ab75e2fcbaeaf0b6
Vitalya <zmuskovitaliy@gmail.com>
2021-03-17 18:56:29 +0300

    jd01-08 full

--- src/by/it/zmushko/jd01_09/Matrix.java
+++ src/by/it/zmushko/jd01_09/Matrix.java
@@ -1,4 +1,4 @@
-package by.it.zmushko.jd01_08;
+package by.it.zmushko.jd01_09;
 
 import java.util.Arrays;
 
---
+++ src/by/it/zmushko/jd01_09/VarCreator.java
@@ -0,0 +1,20 @@
+package by.it.zmushko.jd01_09;
+
+class VarCreator {
+
+    private VarCreator() {
+    }
+
+    static Var build(String strVar){
+        strVar = strVar.replaceAll(" ", "");
+        if (strVar.matches(Patterns.SCALAR)) {
+            return new Scalar(strVar);
+        } else if (strVar.matches(Patterns.VECTOR)) {
+            return new Vector(strVar);
+        } else if (strVar.matches(Patterns.MATRIX)) {
+            return new Matrix(strVar);
+        } else {
+            return null; //TODO В ЛИЦО ОШИБКУ
+        }
+    }
+}
