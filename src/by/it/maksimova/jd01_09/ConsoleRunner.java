a21c60a1fb62a912469024a95b384cb4a4f44591
maksimova <ps_nat98@mail.ru>
2021-03-17 11:53:06 +0300

    jd01-07

---
+++ src/by/it/maksimova/jd01_09/ConsoleRunner.java
@@ -0,0 +1,21 @@
+package by.it.maksimova.jd01_09;
+
+import java.util.Scanner;
+
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Parser parser = new Parser();
+        Scanner scanner = new Scanner(System.in);
+        for (;;) { // значит while (true)
+            String expression = scanner.nextLine();
+            if (!expression.equals("end")) {
+                Var resultVar = parser.evaluate(expression);
+                printer.print(resultVar);
+            } else {
+                break;
+            }
+        }
+
+    }
+}
---
+++ src/by/it/maksimova/jd01_09/Scalar.java
@@ -0,0 +1,69 @@
+package by.it.maksimova.jd01_09;
+
+public class Scalar extends Var {
+
+    private final double value;
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+          double sum = this.value+((Scalar) other).value;
+            return new Scalar(sum);
+//            такая же запись ниже
+//            return new Scalar(this.value + ((by.it._classwork_.jd01_08.Scalar) other).value);
+        }
+        return other.add(this);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar) {
+            double sub = this.value-((Scalar) other).value;
+            return new Scalar(sub);
+        }
+        return new Scalar(-1).mul(other).add(this);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar) {
+            double mul = this.value*((Scalar) other).value;
+            return new Scalar(mul);
+        }
+        return other.mul(this);
+    }
+
+    @Override
+    public Var div(Var other) {
+        if (other instanceof Scalar) {
+            double secondScalar=((Scalar) other).value;
+            if (secondScalar==0){
+                return null; //TODO div by zero
+            }
+            return new Scalar(this.value / secondScalar);
+        }
+        return super.div(other);
+    }
+
+    // первый конструктор - возвращать строку из вещественного числа
+     Scalar(double value) {
+        this.value = value;
+    }
+// второй конструктор из строки вида 3.1415 в double
+     Scalar(String strValue) {
+        this.value=Double.parseDouble(strValue);
+    }
+// из точно такой же переменной
+     Scalar(Scalar otherScalar) {
+        this.value= otherScalar.value;
+    }
+// переопредление метода String toString() из Object
+    @Override
+    public String toString() {
+        return Double.toString(value);
+    }
+}
