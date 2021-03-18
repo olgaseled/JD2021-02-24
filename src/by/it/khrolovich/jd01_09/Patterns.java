cdeb606418810150a5520bca638549af14d9bc0f
MarinaKhrolovich <maniuneshka@gmail.com>
2021-03-19 00:32:35 +0300

    jd01_09 classwork

---
+++ src/by/it/khrolovich/jd01_09/Patterns.java
@@ -0,0 +1,16 @@
+package by.it.khrolovich.jd01_09;
+
+interface Patterns {
+    String OPERATION = "[-+*/]";
+    String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
+    //String Vector = "\{-?[0-9]+(\.[0-9]+)?(,-?[0-9]+(\.[0-9]+)?)*}";
+    String VECTOR = "\\{"+SCALAR+"(,"+SCALAR+"*}";
+    String MATRIX = "\\{"+VECTOR+"(,"+VECTOR+"*}";
+
+    /*11+12
+    2+2.0
+    {1,3,-5.0}+4.0
+    {1,3,5.0}+{2,3,4}
+    {{1,3,5.0},{3,4}}*{1.0,2}
+    */
+}
