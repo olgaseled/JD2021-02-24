c4c4d43a0b7c7f6a9316a3346ff02f3435904f51
Andrey <and.rey882012@gmail.com>
2021-03-18 22:14:36 +0300

    18.03.2021

--- src/by/it/belazarovich/jd01_09/Var.java
+++ src/by/it/belazarovich/jd01_09/Var.java
@@ -1,16 +1,17 @@
 package by.it.belazarovich.jd01_09;
 
+
 abstract class Var implements Operation {
 
     static Var createVar(String operand) {
         operand = operand.trim().replace("\\s+", "");
         if (operand.matches(Patterns.SCALAR))
             return new Scalar(operand);
-        if (operand.matches(Patterns.VECTOR))
-            return new Scalar(operand);
-        if (operand.matches(Patterns.MATRIX))
-            return new Scalar(operand);
-return null;
+        //      if (operand.matches(Patterns.VECTOR))
+        //         return new Vector(operand);
+        //      if (operand.matches(Patterns.MATRIX))
+        //           return new Matrix(operand);
+        return null;
 
 
     }
@@ -24,19 +25,19 @@ return null;
 
     @Override
     public Var sub(Var other) {
-        System.out.println("Операция сложения" + this + "+" + other + "невозможна");
+        System.out.println("Операция вычитания" + this + "-" + other + "невозможна");
         return null;
     }
 
     @Override
     public Var mul(Var other) {
-        System.out.println("Операция сложения" + this + "+" + other + "невозможна");
+        System.out.println("Операция умножения" + this + "*" + other + "невозможна");
         return null;
     }
 
     @Override
     public Var div(Var other) {
-        System.out.println("Операция сложения" + this + "+" + other + "невозможна");
+        System.out.println("Операция деления" + this + "/" + other + "невозможна");
         return null;
     }
 }
