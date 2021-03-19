5ecbd9e7b4bfd5162c2b37b1303331b948200291
Andrey <and.rey882012@gmail.com>
2021-03-19 12:09:42 +0300

    19.03.2021

---
+++ src/by/it/belazarovich/jd01_10/Bean.java
@@ -0,0 +1,27 @@
+package by.it.belazarovich.jd01_10;
+/*
+public class Bean {
+    @Param(a = 99, b = 19)
+    double sum(int aa, int bb) {
+        return aa + bb;
+    }
+
+    static double sum(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    @Param(int a, int b) {
+        return a < b ? a : b;
+    }
+
+
+    double sum(int a, int b) {
+        return a > b ? a : b;
+    }
+
+
+}
+
+
+
+ */
---
+++ src/by/it/belazarovich/jd01_10/PrintMath.java
@@ -0,0 +1,14 @@
+package by.it.belazarovich.jd01_10;
+/*
+import java.lang.reflect.Method;
+
+public class PrintMath {
+    public static void main(String[] args) {
+        Class<Math> mathClass = Math.class;
+        Method[]=
+        metods
+    }
+}
+
+
+ */
