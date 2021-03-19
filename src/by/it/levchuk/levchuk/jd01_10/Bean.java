f1c06bb0bd38e66afa648c4326eb746f6f0abcd3
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-19 11:37:44 +0300

    jd01-10

---
+++ src/by/it/levchuk/levchuk/jd01_10/Bean.java
@@ -0,0 +1,20 @@
+package by.it.levchuk.levchuk.jd01_10;
+
+class Bean {
+    //@Param(a=9, b=18)
+    //double sum(int a, int b){
+    //    return a+b;
+    //}
+    //@Param(a=9, b=18)
+    //double sum(int a, int b){
+    //    return a>b?a:b;
+    //}
+   // @Param(a=9, b=18)
+    //double sum(int a, int b){
+    //    return a<b?a:b;
+    //}
+   // @Param(a=9, b=18)
+   // double sum(int a, int b){
+   //     return (a+b)/2.0;
+   // }
+}
