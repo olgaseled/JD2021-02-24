bb7a43cc0e0c8fe889c740b25eb1614ed45c63e1
MarinaKhrolovich <maniuneshka@gmail.com>
2021-03-19 20:50:42 +0300

    JD01_10 finished TaskB

---
+++ src/by/it/khrolovich/jd01_10/BeanTester.java
@@ -0,0 +1,7 @@
+package by.it.khrolovich.jd01_10;
+
+public class BeanTester {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
--- src/by/it/khrolovich/jd01_10/PrintMath.java
+++ src/by/it/khrolovich/jd01_10/PrintMath.java
@@ -1,18 +1,19 @@
 package by.it.khrolovich.jd01_10;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 
-public class PrintMath {
+class PrintMath {
     public static void main(String[] args) {
         Class<Math> mathClass = Math.class;
+        //1.Публичные методы
         Method[] methods = mathClass.getDeclaredMethods();
-
         for (Method method : methods) {
             StringBuilder contract = new StringBuilder();
-            System.out.println(method);
+            //System.out.println(method);
             int modifiers = method.getModifiers();
             //только публичные
            /* if((modifiers&1)!=0){
@@ -42,19 +43,43 @@ public class PrintMath {
 
             Class<?> returnType = method.getReturnType();
             contract
-                    .append(returnType.getSimpleName())
+                    .append(returnType.getName())
                     .append(' ')
                     .append(method.getName())
                     .append('(');
             Class<?>[] parameterTypes = method.getParameterTypes();
             String delimiter = "";
             for (Class<?> parameterType : parameterTypes) {
-                contract.append(delimiter).append(parameterType.getSimpleName());
-                delimiter = ", ";
+                contract.append(delimiter).append(parameterType.getName());
+                delimiter = ",";
             }
             contract.append(')');
-
+            System.out.println(contract);
         }
+        //публичные поля
+        Field[] fields = mathClass.getDeclaredFields();
 
+        for (Field field : fields) {
+            StringBuilder contract = new StringBuilder();
+            int modifiers = field.getModifiers();
+
+            if (Modifier.isPublic(modifiers)) {
+                contract.append("public ");
+            } else {
+                continue;
+            }
+            if (Modifier.isPrivate(modifiers)) {
+                contract.append("private ");
+            }
+            if (Modifier.isProtected(modifiers)) {
+                contract.append("protected ");
+            }
+            if (Modifier.isStatic(modifiers)) {
+                contract.append("static ");
+            }
+
+            contract.append(field.getType()).append(" ").append(field.getName());
+            System.out.println(contract);
+        }
     }
 }
