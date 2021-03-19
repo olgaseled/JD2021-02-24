3235cf2d90ca29ff5f69a7376b05407e88e9c41f
Vitalya <zmuskovitaliy@gmail.com>
2021-03-19 11:16:07 +0300

    jd01-09

--- src/by/it/zmushko/jd01_09/Patterns.java
+++ src/by/it/zmushko/jd01_09/Patterns.java
@@ -2,7 +2,7 @@ package by.it.zmushko.jd01_09;
 
 interface Patterns {
 
-    String OPERATION = "[-+*/]";
+    String OPERATION = "[-+*/]"; //минус в начале или в конце что бы не поломать
     String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
     String VECTOR = "\\{" + SCALAR + "(," + SCALAR + ")*}";
     String MATRIX = "\\{" + VECTOR + "(," + VECTOR + ")*}";
