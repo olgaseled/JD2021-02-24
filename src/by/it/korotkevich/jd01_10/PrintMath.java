dccc0bbd101d7a40f47e50ae113fc47e8e50f446
katsia-ryna <kati.18@mail.ru>
2021-03-19 12:08:52 +0300

    jd01_08

---
+++ src/by/it/korotkevich/jd01_10/PrintMath.java
@@ -0,0 +1,44 @@
+package by.it.korotkevich.jd01_10;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+class PrintMath {
+    public static void main(String[] args) {
+        Class<Math> mathClass = Math.class;
+        Method[] methods = mathClass.getDeclaredMethods();
+        for (Method method : methods) {
+            StringBuilder contract = new StringBuilder();
+            int modifiers = method.getModifiers();
+            if (Modifier.isPublic(modifiers)) {
+                contract.append("public ");
+            } else {
+                continue;
+            }
+            if (Modifier.isPrivate(modifiers)) {
+                contract.append("private ");
+            }
+            if (Modifier.isStatic(modifiers)) {
+                contract.append("static ");
+            }
+            if (Modifier.isProtected(modifiers)) {
+                contract.append("protected ");
+            }
+            Class<?> returnType = method.getReturnType();
+            contract
+                    .append(returnType.getSimpleName())
+                    .append(' ')
+                    .append(method.getName())
+                    .append('(');
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            String delimeter="";
+            for (Class<?> parameterType : parameterTypes) {
+                contract.append(delimeter).append(parameterType.getSimpleName());
+                delimeter=",";
+            }
+            contract.append(")");
+            System.out.println(contract);
+            //TODO доделать поля (double E, double PI и т.д.)
+        }
+    }
+}
