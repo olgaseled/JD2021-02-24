84f6451e63657d69f3916938529ac61f98bb09d0
seledtsova <olgaseledtsova9@yandex.by>
2021-03-19 12:14:05 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2021-02-24
     Conflicts:
    	src/by/it/seledtsova/jd01_09/ConsoleRunner.java
    	src/by/it/seledtsova/jd01_09/Matrix.java
    	src/by/it/seledtsova/jd01_09/Operation.java
    	src/by/it/seledtsova/jd01_09/Parser.java
    	src/by/it/seledtsova/jd01_09/Patterns.java
    	src/by/it/seledtsova/jd01_09/Printer.java
    	src/by/it/seledtsova/jd01_09/Scalar.java
    	src/by/it/seledtsova/jd01_09/Test_jd01_09.java
    	src/by/it/seledtsova/jd01_09/Var.java
    	src/by/it/seledtsova/jd01_09/VarCreator.java
    	src/by/it/seledtsova/jd01_09/Vector.java

---
+++ src/by/it/seledtsova/jd01_10/PrintMath.java
@@ -0,0 +1,43 @@
+package by.it.seledtsova.jd01_10;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class PrintMath {
+    /*
+}
+    public static void main(String[] args) { //Class<?> stringClass = String.class;
+    Class<Math> mathClass = Math.class; // ctrl-alt-v  или .var ( посмотри в refactor -extract/introduce )
+        Method[] methods = mathClass.getDeclaredMethods();
+        for (Method method: methods) {
+            StringBuilder contract = new StringBuilder();
+            int modifiers = method.getModifiers();
+            // System.out.println(method); // напечатались все методы , которые есть
+            if (Modifier.isPrivate(modifiers)) {
+                contract.append("private");
+            }
+            if (Modifier.isStatic(modifiers)) {
+                contract.append("static");
+            }
+            if (Modifier.isPublic(modifiers)) {
+                contract.append("public");
+            } esle
+                        continue;
+
+            if (Modifier.isProtected(modifiers))
+                contract.append("protected"); {
+            }
+            Class <?> returnTupe = method.getReturnType();
+            contract
+                    .append(returnTupe.getSimpleName())
+                    .append(' ')
+                    .append(method.getName())
+                    .append('(');
+           Class <?> [] parameterTypes=method
+                    String delimiter=",";
+           for (Class <?>) paramertTypes
+
+
+
+     */
+}
