570b2a43f1a231074cce149f7b1a19463983830f
Илья <ilya.papruga@gmail.com>
2021-03-20 13:45:54 +0300

    jd01_07 C Complete

--- src/by/it/papruga/jd01_07/Matrix.java
+++ src/by/it/papruga/jd01_07/Matrix.java
@@ -5,30 +5,47 @@ import java.util.StringJoiner;
 public class Matrix extends Var {
 
 
-    private double[][] value;
+    final private double[][] value;
 
-    public Matrix(double[][] value) {
+    Matrix(double[][] value) {
 
         this.value = value;
     }
 
-    public Matrix(Matrix otherMatrix) {
+    Matrix(Matrix otherMatrix) {
 
         this.value = otherMatrix.value;
     }
 
-    public Matrix(String strMatrix) {
+    Matrix(String strMatrix) {
 
 
+        String[] matrix = strMatrix.replaceAll(" ", "").split("[}]+[,]+[{]");
+
+        matrix[0] = matrix[0].replaceAll("[{}]", "");
+        matrix[1] = matrix[1].replaceAll("[{}]", "");
+
+        String[] vector1 = matrix[0].split("[,]");
+        String[] vector2 = matrix[1].split("[,]");
+
+        double[][] mas = new double[vector1.length][vector2.length];
+
+        for (int i = 0; i < vector1.length; i++) {
+            mas[0][i] = Double.parseDouble(vector1[i]);
+        }
+        for (int i = 0; i < vector2.length; i++) {
+            mas[1][i] = Double.parseDouble(vector2[i]);
+        }
+
+        this.value = mas;
 
     }
-
 
     @Override
     public String toString() {
 
 
-        StringJoiner stringJoiner = new StringJoiner(", ", "{{", "},");
+        StringJoiner stringJoiner = new StringJoiner(", ", "{{", "}, ");
         for (Double v : value[0]) {
             stringJoiner.add(v.toString());
         }
--- src/by/it/papruga/jd01_07/Scalar.java
+++ src/by/it/papruga/jd01_07/Scalar.java
@@ -1,19 +1,18 @@
 package by.it.papruga.jd01_07;
 
-public class Scalar extends Var {
+class Scalar extends Var {
 
     private final double value;
 
-
-    public Scalar(double value) {
+    Scalar(double value) {
         this.value = value;
     }
 
-    public Scalar(String strValue) {
+    Scalar(String strValue) {
         this.value = Double.parseDouble(strValue);
     }
 
-    public Scalar(Scalar otherScalar) {
+    Scalar(Scalar otherScalar) {
         this.value = otherScalar.value;
     }
 
