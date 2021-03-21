d451a5b80c845316f7f16e6d47178415421dbbfb
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-21 19:05:15 +0300

    jd01-10 Home_work

--- src/by/it/levchuk/levchuk/jd01_10/PrintMath.java
+++ src/by/it/levchuk/levchuk/jd01_10/PrintMath.java
@@ -1,18 +1,15 @@
 package by.it.levchuk.levchuk.jd01_10;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class PrintMath {
     public static void main(String[] args) {
-        Class<?> mathClass = Math.class;
+        Class<Math> mathClass = Math.class;
         Method[] methods = mathClass.getDeclaredMethods();
-        for (Method method:methods);{
-           // System.out.println(method);
-           // int modifires = methods.getClass();
-            //8==0b//
-           // if ((modifires & 1)!=0);
-           // System.out.println(method);
+        for (Method method : methods) {
+            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
+                System.out.println(method);
         }
-
     }
 }
