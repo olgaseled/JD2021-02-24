96eab9a6b00d0a2143920a46e9c088dbc08e9331
seledtsova <olgaseledtsova9@yandex.by>
2021-03-21 18:57:45 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2021-02-24
     Conflicts:
    	src/by/it/seledtsova/jd01_09/ConsoleRunner.java
    	src/by/it/seledtsova/jd01_09/Matrix.java
    	src/by/it/seledtsova/jd01_09/Operation.java
    	src/by/it/seledtsova/jd01_09/Parser.java
    	src/by/it/seledtsova/jd01_09/Patterns.java
    	src/by/it/seledtsova/jd01_09/Printer.java
    	src/by/it/seledtsova/jd01_09/Scalar.java
    	src/by/it/seledtsova/jd01_09/Test_jd01_09.java
    	src/by/it/seledtsova/jd01_09/Var.java
    	src/by/it/seledtsova/jd01_09/VarCreator.java
    	src/by/it/seledtsova/jd01_09/Vector.java

--- src/by/it/seledtsova/jd01_10/PrintMath.java
+++ src/by/it/seledtsova/jd01_10/PrintMath.java
@@ -9,57 +9,15 @@ public class PrintMath {
         Class<Math> structMath = Math.class; //мы получили класс Math и занесли нам класс в переменную
         Method[] methods = structMath.getDeclaredMethods();// написали команду, для всех методов. ЗАписана как массив методов
         for (Method method : methods) { // переберем все методы
-            if ((method.getModifiers() &Modifier.PUBLIC) ==Modifier.PUBLIC) // выясним , какие из методов являются статическими.
-                                        // Получим из каждого метода его модификатор. Если то, что у нас получилось не равняяется
-                                       // !=Modifier.STATIC, то под if достанутся все не статические методы класса. ( их нет у нас)
-                                        // == если заменить != на ==, то все статические
-                                        //если модификатор заменить из STATIC на PUBLIC
-                System.out.println(method);
-
+            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
+                System.out.println(method.toString().replace("java.lang.Math.", ""));
         }
+        System.out.println("double E");
+        System.out.println("double PI");
 
     }
-
-
-
-
-
-
-
 }
-    /*
-}
-    public static void main(String[] args) { //Class<?> stringClass = String.class;
-    Class<Math> mathClass = Math.class; // ctrl-alt-v  или .var ( посмотри в refactor -extract/introduce )
-        Method[] methods = mathClass.getDeclaredMethods();
-        for (Method method: methods) {
-            StringBuilder contract = new StringBuilder();
-            int modifiers = method.getModifiers();
-            // System.out.println(method); // напечатались все методы , которые есть
-            if (Modifier.isPrivate(modifiers)) {
-                contract.append("private");
-            }
-            if (Modifier.isStatic(modifiers)) {
-                contract.append("static");
-            }
-            if (Modifier.isPublic(modifiers)) {
-                contract.append("public");
-            } esle
-                        continue;
-
-            if (Modifier.isProtected(modifiers))
-                contract.append("protected"); {
-            }
-            Class <?> returnTupe = method.getReturnType();
-            contract
-                    .append(returnTupe.getSimpleName())
-                    .append(' ')
-                    .append(method.getName())
-                    .append('(');
-           Class <?> [] parameterTypes=method
-                    String delimiter=",";
-           for (Class <?>) paramertTypes
-
-
-
-     */
+// выясним , какие из методов являются статическими.
+// Получим из каждого метода его модификатор. Если то, что у нас получилось не равняяется
+// !=Modifier.STATIC, то под if достанутся все не статические методы класса. ( их нет у нас)
+// == если заменить != на ==, то все статические
