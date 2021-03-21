c01b5c3592f2a28c3bc3880e92e4bfdae80959cf
seledtsova <olgaseledtsova9@yandex.by>
2021-03-21 16:01:14 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2021-02-24
     Conflicts:
    	src/by/it/seledtsova/jd01_09/ConsoleRunner.java
    	src/by/it/seledtsova/jd01_09/Matrix.java
    	src/by/it/seledtsova/jd01_09/Operation.java
    	src/by/it/seledtsova/jd01_09/Parser.java
    	src/by/it/seledtsova/jd01_09/Patterns.java
    	src/by/it/seledtsova/jd01_09/Printer.java
    	src/by/it/seledtsova/jd01_09/Scalar.java
    	src/by/it/seledtsova/jd01_09/Test_jd01_09.java
    	src/by/it/seledtsova/jd01_09/Var.java
    	src/by/it/seledtsova/jd01_09/VarCreator.java
    	src/by/it/seledtsova/jd01_09/Vector.java

--- src/by/it/seledtsova/jd01_10/Bean.java
+++ src/by/it/seledtsova/jd01_10/Bean.java
@@ -1,22 +1,23 @@
 package by.it.seledtsova.jd01_10;
 
+//TaskA2.Создайте класс Bean которыйсодержитдвастатическихи дванестатическихметода.Имена sum max min avg с сигнатурой
+// (int a, int b), какие именно из них статические выберите произвольно.Методы должны вычислять для своих параметров
+// a и b сумму, максимум, минимум и среднее и возвращать результат какdouble.Трииз этих методов должны быть помечены
+// ранее созданной аннотациейс разными значениями a и b
+
  class Bean {
-     @Param(a=9, b=18)
-     double sum (int a, int b) {
+
+     @Param(a=2, b=5)
+     static double sum (int a,int b) {
          return a+b;
-         }
-
-     static  double max ( int a, int b) {
-         return a>b?a:b;
      }
-     @Param(a=9, b=18)
-     static double min ( int a, int b) {
-         return a<b?a:b;
+     static double avg (int a,int b) {
+         return (a+b)/2.0;
      }
-
-     @Param(a=9, b=18)
-     double avg ( int a, int b) {
-         return (a+b) / 2.0;
+     double min (int a,int b) {
+         return a<b? a:b; // если оказалось а<b, то мы возвращаем а, иначе b
      }
-
+     double max (int a,int b) { //если оказалось а>b,то мы возвращаем b, иначе a
+         return a>b? a:b;
+     }
 }
--- src/by/it/seledtsova/jd01_10/Param.java
+++ src/by/it/seledtsova/jd01_10/Param.java
@@ -6,10 +6,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD) // цель
-@Retention(RetentionPolicy.RUNTIME)
-@interface Param {
-    int a();
 
+@Retention(RetentionPolicy.RUNTIME) //  аннотация применяется в момент работы программы
+@Target(ElementType.METHOD) // указали на что распространяется аннотация
+public @interface Param {
+    int a(); // параменты аннотации указываются также как и методы. если а имеет например зачение, то int a () default 100
     int b();
+
 }
+  //  TaskA1.Создайте собственную аннотацию Param, которая описывает параметры для некоторой
+  //  процедуры тестирования произвольного методас сигнатурой (int a, int b),
+//  помеченного такой аннотацией.Пример использования:@Param(a=2, b=5)public double avg(int a, int b) { … }.
