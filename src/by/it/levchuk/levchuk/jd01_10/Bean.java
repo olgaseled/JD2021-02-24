472b0a8e7d26444b741ef733cdfa4dbf223cc21d
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-21 18:21:37 +0300

    jd01-10 Home_work

--- src/by/it/levchuk/levchuk/jd01_10/Bean.java
+++ src/by/it/levchuk/levchuk/jd01_10/Bean.java
@@ -1,20 +1,23 @@
 package by.it.levchuk.levchuk.jd01_10;
 
-class Bean {
-    //@Param(a=9, b=18)
-    //double sum(int a, int b){
-    //    return a+b;
-    //}
-    //@Param(a=9, b=18)
-    //double sum(int a, int b){
-    //    return a>b?a:b;
-    //}
-   // @Param(a=9, b=18)
-    //double sum(int a, int b){
-    //    return a<b?a:b;
-    //}
-   // @Param(a=9, b=18)
-   // double sum(int a, int b){
-   //     return (a+b)/2.0;
-   // }
+public class Bean {
+    @Param(a = 7, b = 15)
+    double sum(int a, int b) {
+        return a + b;
+    }
+
+    @Param(a = 7, b = 15)
+    double max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    @Param(a = 7, b = 15)
+    static double min(int a, int b) {
+        return a < b ? a : b;
+    }
+
+    static double avg(int a, int b) {
+        return (a + b) / 2.0;
+    }
+
 }
