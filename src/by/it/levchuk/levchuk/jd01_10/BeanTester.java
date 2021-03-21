f058915f9da9dd265f85f72bb2803307b6f8f58d
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-21 20:53:03 +0300

    jd01-10 Home_work

---
+++ src/by/it/levchuk/levchuk/jd01_10/BeanTester.java
@@ -0,0 +1,23 @@
+package by.it.levchuk.levchuk.jd01_10;
+
+import java.lang.reflect.Method;
+
+public class BeanTester {
+    public static void main(String[] args) throws Exception {
+        Class<Bean> beanClass=Bean.class;
+        Method[] methods = beanClass.getDeclaredMethods();
+        Object object = null;
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Param.class)) {
+                if (object == null) {
+                    object = beanClass.getConstructor().newInstance();
+                }
+                Param bean = method.getAnnotation(Param.class);
+                Object invoke = method.invoke(object, bean.a(), bean.b());
+
+                System.out.println(method.getName() + " " + invoke.toString());
+            }
+
+        }
+    }
+}
--- src/by/it/levchuk/levchuk/jd01_10/PrintMath.java
+++ src/by/it/levchuk/levchuk/jd01_10/PrintMath.java
@@ -1,15 +1,79 @@
 package by.it.levchuk.levchuk.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
+
+
 public class PrintMath {
     public static void main(String[] args) {
+        printMathMethods();
+        printMathFields();
+    }
+
+    public static void printMathMethods() {
         Class<Math> mathClass = Math.class;
         Method[] methods = mathClass.getDeclaredMethods();
         for (Method method : methods) {
-            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
-                System.out.println(method);
+            StringBuilder contr = new StringBuilder();
+            int modifiers = method.getModifiers();
+            if (Modifier.isPublic(modifiers)) {
+                contr.append("public ");
+            } else {
+                continue;
+            }
+            if (Modifier.isPrivate(modifiers)) {
+                contr.append("private ");
+            }
+            if (Modifier.isStatic(modifiers)) {
+                contr.append("static ");
+            }
+            if (Modifier.isProtected(modifiers)) {
+                contr.append("protected ");
+            }
+            Class<?> returnType = method.getReturnType();
+            contr
+                    .append(returnType.getSimpleName())
+                    .append(' ')
+                    .append(method.getName())
+                    .append('(');
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            String delimeter = "";
+            for (Class<?> parameterType : parameterTypes) {
+                contr.append(delimeter).append(parameterType.getSimpleName());
+                delimeter = ",";
+            }
+            contr.append(")");
+            System.out.println(contr);
+        }
+    }
+
+    public static void printMathFields() {
+        Class<Math> mathClass = Math.class;
+        Field[] fields = mathClass.getDeclaredFields();
+        for (Field field : fields) {
+            StringBuilder contr = new StringBuilder();
+            int modifiers = field.getModifiers();
+            if (Modifier.isPublic(modifiers)) {
+                contr.append("public ");
+            } else {
+                continue;
+            }
+            if (Modifier.isPrivate(modifiers)) {
+                contr.append("private ");
+            }
+            if (Modifier.isStatic(modifiers)) {
+                contr.append("static ");
+            }
+            if (Modifier.isProtected(modifiers)) {
+                contr.append("protected ");
+            }
+            contr
+                    .append(field.getType())
+                    .append(' ')
+                    .append(field.getName());
+            System.out.println(contr);
         }
     }
 }
