23889feac2d84ab630528246354a66554fb01b30
Oleg Zh <ffieryiceadd@gmail.com>
2021-03-22 12:24:32 +0300

    jd01_11 classwork

---
+++ src/by/it/papruga/jd01_11/Runner.java
@@ -0,0 +1,24 @@
+package by.it.papruga.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+
+        System.out.println(list);
+
+        list.remove("three");
+        System.out.println(list);
+
+        System.out.println(list.get(1));
+
+    }
+}
