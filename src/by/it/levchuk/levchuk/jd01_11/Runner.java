4a8c79a030a9acf177c57554d558b517e4089e56
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-22 12:30:13 +0300

    JD01-11

--- src/by/it/levchuk/levchuk/jd01_11/Runner.java
+++ src/by/it/levchuk/levchuk/jd01_11/Runner.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        List<String> list=new ArrayList<>();
+        List<String> list=new ListA<>();
         list.add("one");
         list.add("two");
         list.add("three");
