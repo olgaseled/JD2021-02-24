b88dd7aaf5c15a8acc1ec1874b0f04b012529af9
MaksimovaNatalya <ps_nat98@mail.ru>
2021-03-24 23:11:07 +0300

    jd01_10

---
+++ src/by/it/maksimova/jd01_11/ListA.java
@@ -0,0 +1,143 @@
+package by.it.maksimova.jd01_11;
+
+import java.util.*;
+
+public class ListA<E> implements List<E> {
+
+    @SuppressWarnings("unchecked")
+    private E[] elements = (E[]) new Object[0];
+
+    private int size;
+
+
+    @Override
+    public boolean add(E element) {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 3 / 2 + 1);
+        }
+        elements[size++] = element;
+        return true;
+    }
+
+    @Override
+    public E remove(int index) {
+        E removeValue = elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - 1 - index);
+        elements[size--]=null;
+        return removeValue;
+    }
+
+    @Override
+    public E get(int index) {
+        return elements[index];
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner stringJoiner = new StringJoiner(", ", "[", "]");
+        for (int i = 0; i < size; i++) {
+            stringJoiner.add(elements[i].toString());
+        }
+        return stringJoiner.toString();
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
