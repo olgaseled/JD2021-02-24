dea6eaf032fa12c81c067492f909e5ccc5d653af
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-24 20:24:48 +0300

    JD01-11 homework

--- src/by/it/levchuk/levchuk/jd01_11/Runner.java
+++ src/by/it/levchuk/levchuk/jd01_11/Runner.java
@@ -5,25 +5,14 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        List<String> myList=new ListA<>();
-        List<String> arList=new ArrayList<>();
-
-        myList.add("one"); arList.add("one");
-        myList.add("two"); arList.add("two");
-        myList.add("three"); arList.add("three");
-        myList.add("four"); arList.add("four");
-
-        System.out.println("myist:"+myList+"\narList:"+arList);
-
-        myList.add(2,"three"); arList.add(2,"three");
-        myList.add(0,"start"); arList.add(0,"start");
-
-        System.out.println("myList:"+myList+"\narList:"+arList);
-
-        myList.remove("start"); arList.remove("start");
-        myList.remove(3); arList.remove(3);
-
-        System.out.println("myList:"+myList+"\narList:"+arList);
-        System.out.println("myList(0):"+myList.get(0)+"\narList(0)"+arList.get(0));
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+        System.out.println(list.get(1));
     }
 }
