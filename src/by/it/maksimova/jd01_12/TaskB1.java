11ea44a2db618b0f094ed05215f5f9e0a250aef0
MaksimovaNatalya <ps_nat98@mail.ru>
2021-03-28 21:07:46 +0300

    jd01_10

---
+++ src/by/it/maksimova/jd01_12/TaskB1.java
@@ -0,0 +1,32 @@
+package by.it.maksimova.jd01_12;
+
+import java.util.*;
+
+public class TaskB1 {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> numberOfWords = new HashMap<>();
+// ввести с консоли текст 100 и более слов
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String words = scanner.nextLine();
+            if (words.equals("end")) {
+                break;
+            }
+// Для каждого слова с помощью коллекций подсчитать частоту его встречаемости
+String[] splittedWords = words.split("[,.:; ]");
+            for (String word : splittedWords) {
+
+                numberOfWords.put(word, numberOfWords.getOrDefault(word, 0) + 1);
+            }
+
+
+        }
+        System.out.println(numberOfWords);
+    }
+
+
+}
+
+
