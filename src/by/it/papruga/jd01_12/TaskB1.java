dfa00c634bd6ea5a2735dbb8297e886cbf737db1
Илья <ilya.papruga@gmail.com>
2021-03-29 00:37:08 +0300

    calc A

---
+++ src/by/it/papruga/jd01_12/TaskB1.java
@@ -0,0 +1,25 @@
+package by.it.papruga.jd01_12;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TaskB1 {
+
+    public static void main(String[] args) {
+
+        HashMap <String, Integer> map = new HashMap<>();
+
+        Scanner scanner = new Scanner(System.in);
+        String text = " ";
+
+        while (!text.equals("end"))
+        {
+            text = scanner.nextLine();
+
+        }
+
+    }
+
+
+
+}
