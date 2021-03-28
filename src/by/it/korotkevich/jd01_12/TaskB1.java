66d09fe362da3b5ba56b4b43044b8d285bfaa59a
Korotkevich Alexey <lyoshakorotkevich0909@gmail.com>
2021-03-28 20:35:31 +0300

    jd01_12 B частично

--- src/by/it/korotkevich/jd01_12/TaskB1.java
+++ src/by/it/korotkevich/jd01_12/TaskB1.java
@@ -1,7 +1,23 @@
 package by.it.korotkevich.jd01_12;
 
+import java.util.*;
+
 public class TaskB1 {
-    /*
-    с помощью HashMap
-     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> words = new HashMap<>();
+        while (true) {
+            String word = scanner.next();
+            if (word.equals("end")) {
+                break;
+            }
+            String trimmedWord = word.trim().replaceAll("[^a-zA-Z']", "");
+            if (!words.containsKey(trimmedWord)) {
+                words.put(trimmedWord, 1);
+            } else {
+                words.put(trimmedWord, words.get(trimmedWord)+1);
+            }
+        }
+        System.out.println(words);
+    }
 }
