8833d1d97fe0a4d29bbe8475024e85da1afca1e3
seledtsova <olgaseledtsova9@yandex.by>
2021-03-28 13:19:34 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2021-02-24
     Conflicts:
    	src/by/it/seledtsova/jd01_09/ConsoleRunner.java
    	src/by/it/seledtsova/jd01_09/Matrix.java
    	src/by/it/seledtsova/jd01_09/Operation.java
    	src/by/it/seledtsova/jd01_09/Parser.java
    	src/by/it/seledtsova/jd01_09/Patterns.java
    	src/by/it/seledtsova/jd01_09/Printer.java
    	src/by/it/seledtsova/jd01_09/Scalar.java
    	src/by/it/seledtsova/jd01_09/Test_jd01_09.java
    	src/by/it/seledtsova/jd01_09/Var.java
    	src/by/it/seledtsova/jd01_09/VarCreator.java
    	src/by/it/seledtsova/jd01_09/Vector.java

--- src/by/it/seledtsova/zadacha/Array1.java
+++ src/by/it/seledtsova/zadacha/Array1.java
@@ -5,32 +5,18 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Array1 {
-    private static Object ArrayListList;
-    private static Object Integer;
+  /*
+            Iterator<Map.Entry<Integer, String>> iterator = hashMap.entrySet().iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
 
-    public static void main(String[] args) {
-        ArrayList<Integer> marks=new ArrayList<Integer>(20);
-        for (int i = 0; i <10; i++) {
-            int element = (int) (Math.random() * 10 + 1);
-            marks.add(element);
         }
-            System.out.println(marks);// заполнить массив,
+    }
 
-  /*  public void clearBad (ArrayListList<Integer> marks)
-        int negativeMarks=3;
-        int positiveMarks=4;
-              // Iterator iterator=marks.iterator();
-        for (Iterator<Integer>iterator=marks.iterator()) {
-
-            
-        }
-
+}
 
    */
-
-            System.out.println(marks);
-                              }
-
         }
 
 
