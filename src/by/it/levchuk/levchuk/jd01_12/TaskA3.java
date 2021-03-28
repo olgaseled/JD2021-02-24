4bd79112e7abc1f03b51c66ddb73542451240ed5
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-28 21:21:25 +0300

    JD01-12 homework

--- src/by/it/levchuk/levchuk/jd01_12/TaskA3.java
+++ src/by/it/levchuk/levchuk/jd01_12/TaskA3.java
@@ -3,13 +3,29 @@ package by.it.levchuk.levchuk.jd01_12;
 /*crash-noobik
 Liauchuk Aliaksandr*/
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TaskA3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true){
+        List<Integer> res = new ArrayList<>();
+        int posZero = 0;
+        while (true) {
             String word = scanner.next();
+            if (word.equals("end")) {
+                break;
+            }
+            Integer integer = Integer.valueOf(word);
+            if (integer < 0) {
+                res.add(integer);
+            } else if (integer == 0) {
+                res.add(posZero, integer);
+            } else {
+                res.add(posZero++, integer);
+            }
         }
+        System.out.println(res);
     }
 }
