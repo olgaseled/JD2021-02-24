d9372216ebf97573437643f50f30847ba9ef66db
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-28 19:15:04 +0300

    JD01-12 homework

--- src/by/it/levchuk/levchuk/jd01_12/TaskA1.java
+++ src/by/it/levchuk/levchuk/jd01_12/TaskA1.java
@@ -3,20 +3,32 @@ package by.it.levchuk.levchuk.jd01_12;
 Liauchuk Aliaksandr*/
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
 public class TaskA1 {
-    private List<Integer> grades = new ArrayList<>();
-    private static Random random=new Random();
+    private final List<Integer> ratingList = new ArrayList<>();
+    private static final Random random = new Random();
+
+    private void clearBad(List<Integer> ratingList) {
+        Iterator<Integer> iterator = ratingList.iterator();
+        while (iterator.hasNext()) {
+            int rating = iterator.next();
+            if (rating < 4)
+                iterator.remove();
+        }
+    }
 
     public static void main(String[] args) {
         TaskA1 instance = new TaskA1();
         for (int i = 0; i < 20; i++) {
-            int grade= random.nextInt(10)+1;
-            instance.grades.add(grade);
+            int rating = random.nextInt(10) + 1;
+            instance.ratingList.add(rating);
         }
-        System.out.println(instance.grades);
+        System.out.println(instance.ratingList);
+        instance.clearBad(instance.ratingList);
+        System.out.println(instance.ratingList);
 
     }
 }
