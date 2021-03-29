d23fe55e13ba313fcdbba3347dade95a3bcf8517
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-03-29 11:53:11 +0300

    JD01-13 classwork calc

--- src/by/it/levchuk/levchuk/calc/CalcException.java
+++ src/by/it/levchuk/levchuk/calc/CalcException.java
@@ -3,4 +3,19 @@ package by.it.levchuk.levchuk.calc;
 Liauchuk Aliaksandr*/
 
 public class CalcException extends Exception {
+    public CalcException() {
+        super();
+    }
+
+    public CalcException(String message) {
+        super("ERROR: " + message);
+    }
+
+    public CalcException(String message, Throwable cause) {
+        super("ERROR: " + message, cause);
+    }
+
+    public CalcException(Throwable cause) {
+        super(cause);
+    }
 }
