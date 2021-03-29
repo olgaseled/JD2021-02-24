46602b994dae39fbbb204bae179cbd8539c3e4ae
alexey <leva@mail.com>
2021-03-29 23:25:19 +0300

    test

--- src/by/it/dudko/jd01_12/TaskB2.java
+++ src/by/it/dudko/jd01_12/TaskB2.java
@@ -4,8 +4,6 @@ import java.util.*;
 
 public class TaskB2 {
 
-    static int everyNthOut = 2;
-
 //    static String process(ArrayList<String> people) {
 //        int counter = 0;
 //        Iterator<String> it = people.iterator();
@@ -25,21 +23,29 @@ public class TaskB2 {
 
     static String process(ArrayList<String> people) {
         int currentRoundPlayersCount = people.size();
-        int i = 1;
-        while (currentRoundPlayersCount >= everyNthOut) {
-            if ((i - 1) % everyNthOut == 0) {
-                people.remove(i);
-            }
-            if (i == currentRoundPlayersCount - 1) { // last in the list
-                // remove second from start
-                i = 1;
-            } else if (i == currentRoundPlayersCount - 2) { // before last in the list
-                // remove first
-                i = 0;
+        int counter = 1;
+        int cursor = 1;
+        while (currentRoundPlayersCount >= 2) {
+            if ((counter - 1) % 2 == 0 || counter == 0) {
+                people.remove(cursor++);
+                if (counter-1 < currentRoundPlayersCount - 2) {
+                    counter++;
+                    continue;
+                }
+                // finish of current round
+                if (counter-1 ==  currentRoundPlayersCount - 2) { // before last in the list
+                    // remove first
+                    counter = 0;
+                } else  { // last in the list
+                    // remove second from start
+                    counter = 1;
+                }
+                // reset cursor
+                cursor = counter;
+                currentRoundPlayersCount = people.size();
             } else {
-                i++;
+                counter++;
             }
-            currentRoundPlayersCount = people.size();
         }
         return people.get(0);
     }
@@ -48,13 +54,13 @@ public class TaskB2 {
     static String process(LinkedList<String> people) {
         int counter = 0;
         Iterator<String> it = people.iterator();
-        while (people.size() >= everyNthOut) {
+        while (people.size() >= 2) {
             if (!it.hasNext()) {
                 it = people.iterator();
             }
             it.next();
             counter++;
-            if (counter == everyNthOut) {
+            if (counter == 2) {
                 it.remove();
                 counter = 0;
             }
@@ -79,30 +85,14 @@ public class TaskB2 {
                 "Ефрасіння", "Казімір", "Кандрат", "Матвей", "Мірон", "Паліна",
                 "Сямён", "Соф'я", "Тарас", "Трафім", "Улляна", "Ціхан", "Ядвіга"};
         Random rnd = new Random(123);
-        for (int i = 0; players.size() < playersNumber; i++) {
+        while (players.size() < playersNumber) {
             int randI = rnd.nextInt(playersNumber) + 1;
-            if (players.contains(localNames[randI])) continue;
+            if (players.contains(localNames[randI])) {
+                continue;
+            }
             players.add(localNames[randI]);
         }
         return players;
-    }
-
-    public static class GameRuleException extends Exception {
-        public GameRuleException() {
-            super();
-        }
-
-        public GameRuleException(String message) {
-            super(message);
-        }
-
-        public GameRuleException(String message, Throwable cause) {
-            super(message, cause);
-        }
-
-        public GameRuleException(Throwable cause) {
-            super(cause);
-        }
     }
 
 }
