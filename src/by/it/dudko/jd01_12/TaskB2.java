58d88fecc5850c0f2a327b3c4b562671bdd412ad
alexey <leva@mail.com>
2021-03-30 22:19:06 +0300

    feat: [jd01_12] add TaskB1, TaskB2, TaskB3

--- src/by/it/dudko/jd01_12/TaskB2.java
+++ src/by/it/dudko/jd01_12/TaskB2.java
@@ -4,24 +4,7 @@ import java.util.*;
 
 public class TaskB2 {
 
-//    static String process(ArrayList<String> people) {
-//        int counter = 0;
-//        Iterator<String> it = people.iterator();
-//        while (people.size() >= everyNthOut) {
-//            if (!it.hasNext()) {
-//                it = people.iterator();
-//            }
-//            it.next();
-//            counter++;
-//            if (counter == everyNthOut) {
-//                it.remove();
-//                counter = 0;
-//            }
-//        }
-//        return people.get(0);
-//    }
-
-    static String process(ArrayList<String> people) {
+    public static String process(ArrayList<String> people) {
         for (int counter = 1, cursor = 1; people.size() > 1; counter++) {
             if (counter % 2 == 0) {
                 cursor = (cursor >= people.size()) ? cursor % people.size() : cursor;
@@ -32,7 +15,7 @@ public class TaskB2 {
     }
 
 
-    static String process(LinkedList<String> people) {
+    public static String process(LinkedList<String> people) {
         int counter = 0;
         Iterator<String> it = people.iterator();
         while (people.size() >= 2) {
@@ -52,7 +35,7 @@ public class TaskB2 {
 
     public static void main(String[] args) {
         int playersNumber = 9;
-        List<String> players = getRandomUniquePlayers(playersNumber);
+        List<String> players = getRandomPlayers(playersNumber, true);
         String winner;
         winner = process(new ArrayList<>(players));
         System.out.println(winner);
@@ -60,15 +43,15 @@ public class TaskB2 {
         System.out.println(winner);
     }
 
-    static List<String> getRandomUniquePlayers(int playersNumber) {
+    public static List<String> getRandomPlayers(int playersNumber, boolean unique) {
         List<String> players = new ArrayList<>(playersNumber * 4 / 3 + 1);
         String[] localNames = {"Алесь", "Анюта", "Васіль", "Віталіна", "Ганна", "Генадзь",
                 "Ефрасіння", "Казімір", "Кандрат", "Матвей", "Мірон", "Паліна",
                 "Сямён", "Соф'я", "Тарас", "Трафім", "Улляна", "Ціхан", "Ядвіга"};
         Random rnd = new Random(123);
         while (players.size() < playersNumber) {
-            int randI = rnd.nextInt(playersNumber) + 1;
-            if (players.contains(localNames[randI])) {
+            int randI = (rnd.nextInt(playersNumber) + 1) % localNames.length;
+            if (unique && players.contains(localNames[randI])) {
                 continue;
             }
             players.add(localNames[randI]);
--- src/by/it/dudko/jd01_12/TaskB3.java
+++ src/by/it/dudko/jd01_12/TaskB3.java
@@ -1,6 +1,67 @@
 package by.it.dudko.jd01_12;
 
+import java.util.*;
+
 public class TaskB3 {
+
+    public static void main(String[] args) {
+        int playersNumber = 4096;
+        List<String> players = getRandomPlayers(playersNumber, false);
+        String winner;
+
+        Timer timer = new Timer(); // set iniTime
+        winner = process(new ArrayList<>(players));
+        System.out.print("Победитель: " + winner + "\n");
+        System.out.print("Время для ArrayList: " + timer.toString() + "\n\n"); // iniTime updates here
+
+        winner = process(new LinkedList<>(players));
+        System.out.print("Победитель: " + winner + "\n");
+        System.out.print("Время для LinkedList: " + timer.toString() + "\n\n");
+    }
+
+    public static String process(ArrayList<String> people) {
+        for (int counter = 1, cursor = 1; people.size() > 1; counter++) {
+            if (counter % 2 == 0) {
+                cursor = (cursor >= people.size()) ? cursor % people.size() : cursor;
+                people.remove(cursor++);
+            }
+        }
+        return people.get(0);
+    }
+
+
+    public static String process(LinkedList<String> people) {
+        int counter = 0;
+        Iterator<String> it = people.iterator();
+        while (people.size() >= 2) {
+            if (!it.hasNext()) {
+                it = people.iterator();
+            }
+            it.next();
+            counter++;
+            if (counter == 2) {
+                it.remove();
+                counter = 0;
+            }
+        }
+        return people.get(0);
+    }
+
+    public static List<String> getRandomPlayers(int playersNumber, boolean unique) {
+        List<String> players = new ArrayList<>(playersNumber * 4 / 3 + 1);
+        String[] localNames = {"Алесь", "Анюта", "Васіль", "Віталіна", "Ганна", "Генадзь",
+                "Ефрасіння", "Казімір", "Кандрат", "Матвей", "Мірон", "Паліна",
+                "Сямён", "Соф'я", "Тарас", "Трафім", "Улляна", "Ціхан", "Ядвіга"};
+        Random rnd = new Random(123);
+        while (players.size() < playersNumber) {
+            int randI = (rnd.nextInt(playersNumber) + 1) % localNames.length;
+            if (unique && players.contains(localNames[randI])) {
+                continue;
+            }
+            players.add(localNames[randI]);
+        }
+        return players;
+    }
 
 
     public static class Timer {
@@ -18,7 +79,7 @@ public class TaskB3 {
         public String toString() {
             delta = (double) (System.nanoTime() - iniTime) / 1000;
             iniTime = System.nanoTime();
-            return "Прошло " + getDeltaString() + " сек.";
+            return "прошло " + getDeltaString() + " сек.";
         }
     }
 }
