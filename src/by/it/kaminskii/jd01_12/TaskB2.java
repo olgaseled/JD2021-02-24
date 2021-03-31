6f4a471f02a8018858ef6be578e9cfa6517d6769
vanek323 <ivankaminskii323@gmail.com>
2021-03-31 11:22:34 +0300

    jd01_05

--- src/by/it/kaminskii/jd01_12/TaskB2.java
+++ src/by/it/kaminskii/jd01_12/TaskB2.java
@@ -8,42 +8,51 @@ public class TaskB2 {
         String[] peoples = new String[]{"1", "2", "3",};
         ArrayList<String> peoplesArrayL = new ArrayList<>(Arrays.asList(peoples));
         LinkedList<String> peoplesLinkedL = new LinkedList<>(Arrays.asList(peoples));
-        System.out.println(process(peoplesArrayL));
-        System.out.println(process(peoplesLinkedL));
+//        System.out.println(process(peoplesArrayL));
+//        System.out.println(process(peoplesLinkedL));
 
-    }
-
-    static String process(ArrayList<String> peoples) {
-        Iterator<String > iterator = peoples.iterator();
-
-
-        for (int i = 1; i <peoples.size(); i++) {
-            if(!iterator.hasNext()) {
-                iterator = peoples.iterator();
-            }
-            iterator.next();
-                if (i == 2) {
-                    iterator.remove();
-                    i = 0;
-                }
-        }
-        return peoples.get(0);
-    }
-
-    static String process(LinkedList<String> peoples) {
-        Iterator<String > iterator = peoples.iterator();
-        for (int i = 1; i <peoples.size(); i++) {
-
-            if(iterator.hasNext()) {
-                if (i == 2) {
-                    iterator.remove();
-                    i = 0;
-                }
-                iterator.next();
-            }
-            else if(!iterator.hasNext())iterator = peoples.iterator();
-
-        }
-        return peoples.get(0);
     }
 }
+//    static String process(ArrayList<String> peoples) {
+//        Iterator<String> iterator = peoples.iterator();
+//
+//        int del = 1;
+//
+//        if (iterator.hasNext()) {
+//            iterator.next();
+//            del++;
+//            if(del==2){
+//                iterator.remove();
+//                del=0;
+//            }
+//        }
+//        for (int i = 1; i <peoples.size(); i++) {
+//            if(!iterator.hasNext()) {
+//                iterator = peoples.iterator();
+//            }
+//            iterator.next();
+//                if (i == 2) {
+//                    iterator.remove();
+//                    i = 0;
+//                }
+//        }
+//        return peoples.get(0);
+//    }
+
+//    static String process(LinkedList<String> peoples) {
+//        Iterator<String > iterator = peoples.iterator();
+//        for (int i = 1; i <peoples.size(); i++) {
+//
+//            if(iterator.hasNext()) {
+//                if (i == 2) {
+//                    iterator.remove();
+//                    i = 0;
+//                }
+//                iterator.next();
+//            }
+//            else if(!iterator.hasNext())iterator = peoples.iterator();
+//
+//        }
+//        return peoples.get(0);
+//    }
+//    }
--- src/by/it/kaminskii/jd01_13/TaskC.java
+++ src/by/it/kaminskii/jd01_13/TaskC.java
@@ -1,34 +1,27 @@
 package by.it.kaminskii.jd01_13;/* created by Kaminskii Ivan
  */
-
+/* презентация сегодняшняя решение*/
 import java.util.Arrays;
+import java.util.*;
 import java.util.Scanner;
 
 public class TaskC {
-    static double[] num = new double[1];
-    static int size = 0;
-    static String strnumber = "";
+    static Scanner scanner;
 
     public static void main(String[] args) throws InterruptedException {
-        Scanner scanner = new Scanner(System.in);
-        strnumber = scanner.next();
+        scanner = new Scanner(System.in);
         readData();
     }
 
     static void readData() throws InterruptedException {
-//        try {
-        for (int i = 0; i < num.length; i++) {
-            if (size == num.length) num = Arrays.copyOf(num, (size * 3 / 2 + 1));
-            num[i] = Double.parseDouble(strnumber);
-            System.out.println("rere" +num[i] +"rere");
-            size++;
-//            }
-//        }catch (Exception e){
-//            Thread.sleep(100);
-//            for (int i = 0; i < num.length; i++) {
-//                System.out.println(num[i]);
-//            }
-//        }
+        double[] number =new double[]{Double.parseDouble(scanner.next())};
+        for (int i = 0; i < number.length; i++) {
+            System.out.println(number[i]);
         }
-    }
-}
+        ArrayList<String> strnumber= new ArrayList<>();
+//        strnumber.add();
+
+        }
+
+
+        }
