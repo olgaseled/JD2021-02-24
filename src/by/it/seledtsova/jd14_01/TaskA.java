9d0154b785d870334106cb6fc49174030fb6feea
seledtsova <olgaseledtsova9@yandex.by>
2021-03-31 12:49:21 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2021-02-24
     Conflicts:
    	src/by/it/seledtsova/jd01_09/ConsoleRunner.java
    	src/by/it/seledtsova/jd01_09/Matrix.java
    	src/by/it/seledtsova/jd01_09/Operation.java
    	src/by/it/seledtsova/jd01_09/Parser.java
    	src/by/it/seledtsova/jd01_09/Patterns.java
    	src/by/it/seledtsova/jd01_09/Printer.java
    	src/by/it/seledtsova/jd01_09/Scalar.java
    	src/by/it/seledtsova/jd01_09/Test_jd01_09.java
    	src/by/it/seledtsova/jd01_09/Var.java
    	src/by/it/seledtsova/jd01_09/VarCreator.java
    	src/by/it/seledtsova/jd01_09/Vector.java

---
+++ src/by/it/seledtsova/jd14_01/TaskA.java
@@ -0,0 +1,23 @@
+package by.it.seledtsova.jd14_01;
+
+import java.io.File;
+
+public class TaskA {
+    /*
+    public static void main(String[] args) {
+       String fileName= "dataTaskA.bin";
+        Class<TaskA> currentStructure = TaskA.class;
+
+
+       String rootProject=System.getProperty("user.dir");
+       String src=rootProject+ File.separator+"src"+File.separator;// свойство системы.
+        Class<TaskA> currentStructure = TaskA.class;
+        String relatedDir=currentStructure
+                .getName()
+                .replace(currentStructure.getSimpleName(), "")
+                .replace(".",File.separator)
+                String filePath=src+relatedDir+fileName;
+            }
+
+     */
+}
