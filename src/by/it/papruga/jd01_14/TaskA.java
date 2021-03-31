f36d400f80551d9fe23e348cc4e5eda4dba2acf0
Oleg Zh <ffieryiceadd@gmail.com>
2021-03-31 12:24:28 +0300

    jd01_14 classwork

---
+++ src/by/it/papruga/jd01_14/TaskA.java
@@ -0,0 +1,16 @@
+package by.it.papruga.jd01_14;
+
+import java.io.File;
+
+public class TaskA {
+
+    public static void main(String[] args) {
+
+        String filename = "dataTaskA.bin";
+        String rootProject = System.getProperty("user.dir");
+        String src = rootProject + File.separator+"src"+File.separator;
+        Class<TaskA> currentStructure = TaskA.class;
+
+    }
+
+}
