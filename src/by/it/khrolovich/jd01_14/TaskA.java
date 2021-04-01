ac81e8ceac07498d245920d42a7934ff2f359db0
MarinaKhrolovich <maniuneshka@gmail.com>
2021-04-01 22:48:30 +0300

    JD01_14 TaskA

--- src/by/it/khrolovich/jd01_14/TaskA.java
+++ src/by/it/khrolovich/jd01_14/TaskA.java
@@ -1,10 +1,7 @@
 package by.it.khrolovich.jd01_14;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Random;
+import java.util.*;
 
 public class TaskA {
     private static final String DATA_TASK_A_BIN = "dataTaskA.bin";
@@ -15,16 +12,19 @@ public class TaskA {
         String fileName = DATA_TASK_A_BIN;
         Class<TaskA> currentStructure = TaskA.class;
         String filePath = getFilePath(fileName, currentStructure);
-        try (FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+        //запись
+        /*try (FileOutputStream fileOutputStream = new FileOutputStream(filePath);
              BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
-             DataOutputStream dataOutputStream = new DataOutputStream(bufferedOutputStream)
-        ) {
-            dataOutputStream.writeInt(88);//соответствует X(большое английское)
+             DataOutputStream dataOutputStream = new DataOutputStream(bufferedOutputStream))*/
+        try (DataOutputStream dataOutputStream = new DataOutputStream(
+                new BufferedOutputStream(
+                        new FileOutputStream(filePath)))) {
+            //dataOutputStream.writeInt(88);//соответствует X(большое английское)
             //dataOutputStream.writeInt(88*256*256*256+88*256*256+88*256);//TODO проверить
             Random random = new Random();
             for (int i = 0; i < 20; i++) {
-                int j = random.nextInt(1000);
-                dataOutputStream.writeInt(j);//увиди не числа текстовым редакторои
+                int j = random.nextInt(1000);//или (int)(Math.random()*1000))
+                dataOutputStream.writeInt(j);//увидиv не числа текстовым редакторои
             }
         } catch (IOException e) {
             //e.printStackTrace();//печатаем не принтстэктрейс, а перенаправляем
@@ -39,37 +39,41 @@ public class TaskA {
 
         //чтение
         DataInputStream dataInputStream = null;//
-        try (FileInputStream fileInputStream = new FileInputStream(filePath);
-             BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-             DataInputStream dataInputStream1 = new DataInputStream(bufferedInputStream);) {
+        try (DataInputStream inp = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(filePath)))) {
             List<Integer> list = new ArrayList<>();//TODO записать о
             //ArrayList<Integer> list = new ArrayList<>();
             //dataInputStream1.read();
-
+            while (inp.available() > 0) {
+                int i = inp.readInt();
+                list.add(i);
+            }
 
             //printToConsole
             double sum = 0;
             for (Integer integer : list) {
                 sum += integer;
-                System.out.printf(" %d", integer);
+                System.out.printf("%d ", integer);
             }
             System.out.printf(Locale.ENGLISH, "\navg=%f\n", sum / list.size());
-            //ptintToFile
+
+          /*  //ptintToFile
             getFilePath("", TaskA.class);
             String txtPath = "";
             try (PrintWriter printWriter = new PrintWriter(txtPath)) {
 
             } catch (FileNotFoundException e) {
                 throw new RuntimeException("my message", e);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            }*/
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (dataInputStream != null) {
-            }
         }
+        /*finally {
+            if (dataInputStream != null) {
+                dataInputStream.close();
+            }
+        }*/
 
     }
 
