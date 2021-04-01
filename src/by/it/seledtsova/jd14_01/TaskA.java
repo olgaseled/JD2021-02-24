f718c37ca5e0a9be5475c2bfc32d359a4fa3148f
seledtsova <olgaseledtsova9@yandex.by>
2021-04-01 20:34:38 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2021-02-24
     Conflicts:
    	src/by/it/seledtsova/jd01_09/ConsoleRunner.java
    	src/by/it/seledtsova/jd01_09/Matrix.java
    	src/by/it/seledtsova/jd01_09/Operation.java
    	src/by/it/seledtsova/jd01_09/Parser.java
    	src/by/it/seledtsova/jd01_09/Patterns.java
    	src/by/it/seledtsova/jd01_09/Printer.java
    	src/by/it/seledtsova/jd01_09/Scalar.java
    	src/by/it/seledtsova/jd01_09/Test_jd01_09.java
    	src/by/it/seledtsova/jd01_09/Var.java
    	src/by/it/seledtsova/jd01_09/VarCreator.java
    	src/by/it/seledtsova/jd01_09/Vector.java

--- src/by/it/seledtsova/jd14_01/TaskA.java
+++ src/by/it/seledtsova/jd14_01/TaskA.java
@@ -8,53 +8,86 @@ package by.it.seledtsova.jd14_01;
 Вывести с новой строки их среднее арифметическоеavg=20.123.
 Продублировать вывод в консоль в файл resultTaskA.txt
  */
-import java.io.File;
+import java.io.*;
 
 public class TaskA {
 
 
-}
+    private static String dir (Class<?> cl) { // метод определяет текущую директорию, в кот находится наш класс
+                                            //FILE.separator - это универсальный разделитесь
+     String path=System.getProperty("user.dir")+File.separator+"src"+File.separator; // определяю ПУТЬ , где находится каталог SRC
+     String clDir=cl.getName().replace(cl.getSimpleName(),"").replace(".",File.separator);
+                                            //узнаю полный путь к классу. TaskA сотрется, а "." заменится "\"
+     return  path+clDir; // возвращаем путь и инфу из клааса
+    } //
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
     public static void main(String[] args) {
-       String fileName= "dataTaskA.bin";
-        Class<TaskA> currentStructure = TaskA.class;
-
-
-       String rootProject=System.getProperty("user.dir");
-       String src=rootProject+ File.separator+"src"+File.separator;// свойство системы.
-        Class<TaskA> currentStructure = TaskA.class;
-        String relatedDir=currentStructure
-                .getName()
-                .replace(currentStructure.getSimpleName(), "")
-                .replace(".",File.separator)
-                String filePath=src+relatedDir+fileName;
+        DataOutputStream dos=null;
+        try {
+            dos=new DataOutputStream(new BufferedOutputStream(
+                    new FileOutputStream(
+                            dir(TaskA.class)+"dataTaskA.bin")));
+            for (int i = 0; i < 20; i++) {
+                dos.writeInt((int) (Math.random()*25));
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (dos !=null) {
+                try {
+                    dos.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        try (DataInputStream inp=new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(dir(TaskA.class)+"dataTaskA.bin")));
+             PrintWriter out2=new PrintWriter(new FileWriter(dir(TaskA.class)+"resultTaskA.txt"))
+        ) {
+            double sum=0;
+            double count=0;
+            while (inp.available()>0) {
+                int i=inp.readInt();
+                System.out.print(i+" ");
+                out2.print(i+" ");
+                sum=sum+i;
+                count++;
+            }
+            System.out.println("\navg="+sum/count);
+            out2.print("\navg="+sum/count);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
 
-     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
