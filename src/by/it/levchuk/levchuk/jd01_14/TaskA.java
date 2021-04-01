f7937cf3c226ea073ef1db1436b81796461aa40e
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-01 09:32:23 +0300

    JD01-14 classwork

---
+++ src/by/it/levchuk/levchuk/jd01_14/TaskA.java
@@ -0,0 +1,42 @@
+package by.it.levchuk.levchuk.jd01_14;
+
+/*crash-noobik
+Liauchuk Aliaksandr*/
+
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.nio.Buffer;
+
+public class TaskA {
+    public static void main(String[] args) {
+//        String filename = "dataTaskA.bin";
+//        Class<TaskA> currentStructure = TaskA.class;
+//        String dataTaskPath = getFilePath(filename, currentStructure);
+//        try (FileOutputStream fileOutputStream = new FileOutputStream(dataTaskPath)) {
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        OutputStream fileOutputStream;
+//        // BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(fileOutputStream);
+//        // DataOutputStream dataOutputStream=new DataOutputStream(bufferedOutputStream)){
+//
+//        // } catch (IOException, e) {
+//        //    throw new RuntimeException(e);
+//    }
+//    // System.out.println(dataTaskPath);
+//    //}
+//
+//    private static String getFilePath(String filename, Class<?> currentStructure) {
+//        String rootProject = System.getProperty("user.dir");
+//        String src = rootProject + File.separator + "src" + File.separator;
+//        String relatedDir = currentStructure {
+//            currentStructure.getName()
+//                    .replace(currentStructure.getSimpleName(), "")
+//                    .replace(".", File.separator);
+//            return null;
+//        }
+
+    }
+}
