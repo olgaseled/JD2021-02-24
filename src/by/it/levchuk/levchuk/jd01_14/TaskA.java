7db7cdd8b30114b191e8cba597819b2531b3fb0a
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-01 13:25:34 +0300

    JD01-14 classwork

--- src/by/it/levchuk/levchuk/jd01_14/TaskA.java
+++ src/by/it/levchuk/levchuk/jd01_14/TaskA.java
@@ -3,40 +3,58 @@ package by.it.levchuk.levchuk.jd01_14;
 /*crash-noobik
 Liauchuk Aliaksandr*/
 
-import javax.xml.crypto.Data;
 import java.io.*;
-import java.nio.Buffer;
+
 
 public class TaskA {
-    public static void main(String[] args) {
-//        String filename = "dataTaskA.bin";
-//        Class<TaskA> currentStructure = TaskA.class;
-//        String dataTaskPath = getFilePath(filename, currentStructure);
-//        try (FileOutputStream fileOutputStream = new FileOutputStream(dataTaskPath)) {
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        OutputStream fileOutputStream;
-//        // BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(fileOutputStream);
-//        // DataOutputStream dataOutputStream=new DataOutputStream(bufferedOutputStream)){
-//
-//        // } catch (IOException, e) {
-//        //    throw new RuntimeException(e);
-//    }
-//    // System.out.println(dataTaskPath);
-//    //}
-//
-//    private static String getFilePath(String filename, Class<?> currentStructure) {
-//        String rootProject = System.getProperty("user.dir");
-//        String src = rootProject + File.separator + "src" + File.separator;
-//        String relatedDir = currentStructure {
-//            currentStructure.getName()
-//                    .replace(currentStructure.getSimpleName(), "")
-//                    .replace(".", File.separator);
-//            return null;
-//        }
 
+    protected static String dir(Class<?> cl) {
+        String path = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String clDir = cl.getName()
+                .replace(cl.getSimpleName(), "")
+                .replace(".", File.separator);
+        return path + clDir;
+    }
+
+    public static void main(String[] args) {
+        DataOutputStream dos = null;
+        try {
+            dos = new DataOutputStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream(dir(TaskA.class) + "dataTaskA.bin"))
+            );
+            for (int i = 0; i < 20; i++) {
+                dos.writeInt((int) (Math.random() * 25));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (dos != null) {
+                try {
+                    dos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        try (DataInputStream inp = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(dir(TaskA.class) + "dataTaskA.bin")));
+             PrintWriter out2 = new PrintWriter(new FileWriter(dir(TaskA.class) + "resultTaskA.txt"))
+        ) {
+            double sum = 0;
+            double count = 0;
+            while (inp.available() > 0) {
+                int i = inp.readInt();
+                System.out.print(i + " ");
+                out2.println(i + " ");
+                sum = sum + i;
+                count++;
+            }
+            System.out.println("\navg=" + sum / count);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
