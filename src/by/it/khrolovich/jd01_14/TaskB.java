9bbcb088ec6378267f38ad0390777b6d06ba504b
MarinaKhrolovich <maniuneshka@gmail.com>
2021-04-02 01:42:17 +0300

    JD01_14 TaskB readFromFile is done

--- src/by/it/khrolovich/jd01_14/TaskB.java
+++ src/by/it/khrolovich/jd01_14/TaskB.java
@@ -1,20 +1,35 @@
 package by.it.khrolovich.jd01_14;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskB {
-
     private static final String USER_DIR = "user.dir";
     private static final String SRC = "src";
     private static final String TASK_B_TXT = "Poem.txt";
 
-    String txtFile = getFilePath(TASK_B_TXT, TaskB.class);
-    List<Integer> list =  readFromFile(txtFile);
+    public static void main(String[] args) {
+        String txtFile = getFilePath(TASK_B_TXT, TaskB.class);
+        StringBuilder textPoem =  readFromFile(txtFile);
+    }
+    private static StringBuilder readFromFile(String txtFile) {
+        StringBuilder text = new StringBuilder();
+        try(BufferedReader bufferedReader= new BufferedReader(new FileReader(txtFile))){
 
-    private List<Integer> readFromFile(String txtFile) {
-        return new ArrayList<>();
+            while (bufferedReader.ready()){
+                text.append(bufferedReader.readLine()+" ");
+            }
+            System.out.println(text);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
+
     }
 
     private static String getFilePath(String filename, Class<?> currentStructure) {
@@ -27,3 +42,4 @@ public class TaskB {
         return src + relatedDir + filename;
     }
 }
+
