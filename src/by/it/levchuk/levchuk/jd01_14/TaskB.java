78fa11c5f029d8f683f7a132a7331d80d8d242d2
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-02 09:11:40 +0300

    JD01-14 homework TaskB

--- src/by/it/levchuk/levchuk/jd01_14/TaskB.java
+++ src/by/it/levchuk/levchuk/jd01_14/TaskB.java
@@ -4,18 +4,58 @@ package by.it.levchuk.levchuk.jd01_14;
 Liauchuk Aliaksandr*/
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class TaskB {
 
-    private static String dir(Class<?> cl) {
+    private static String dir() {
         String path = System.getProperty("user.dir") + File.separator + "src" + File.separator;
-        String clDir = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
+        String clDir = TaskB.class.getName()
+                .replace(TaskB.class.getSimpleName(), "")
+                .replace(".", File.separator);
         return path + clDir;
     }
 
     public static void main(String[] args) {
 
-    }
+        try (BufferedReader reader = new BufferedReader
+                (new FileReader(dir() + "Poem.txt"));
 
+             PrintWriter out = new PrintWriter
+                     (new FileWriter
+                             (dir() + "resultTaskB.txt"))
+        ) {
+            String line = reader.readLine();
+
+            StringBuffer text = new StringBuffer();
+
+            while (line != null) {
+
+                text.append(line).append("\n");
+                line = reader.readLine();
+            }
+
+            int words = 0;
+            Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find()) {
+                words++;
+            }
+
+            int marks = 0;
+            Pattern pattern2 = Pattern.compile("[-:,.!]+");
+            Matcher matcher2 = pattern2.matcher(text);
+            while (matcher2.find()) {
+                marks++;
+            }
+
+            System.out.println("words=" + words + ", punctuation marks=" + marks);
+            out.println("words=" + words + ", punctuation marks=" + marks);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
