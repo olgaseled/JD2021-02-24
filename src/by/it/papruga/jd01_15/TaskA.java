56d57c69e696b24c62e46459a887d5ad492d4b37
Oleg Zh <ffieryiceadd@gmail.com>
2021-04-02 12:36:24 +0300

    jd01_15 classwork

---
+++ src/by/it/papruga/jd01_15/TaskA.java
@@ -0,0 +1,17 @@
+package by.it.papruga.jd01_15;
+
+import java.util.Random;
+
+public class TaskA {
+
+    public static void main(String[] args) {
+
+
+        int[][] matrix = new int [6][4];
+
+
+
+    }
+
+
+}
