ad4973fca6aeecda0553c26e373426ca5e9ae132
vanek323 <ivankaminskii323@gmail.com>
2021-04-02 22:00:56 +0300

    jd01_05

---
+++ src/by/it/kaminskii/jd01_15/TaskA.java
@@ -0,0 +1,22 @@
+package by.it.kaminskii.jd01_15;/* created by Kaminskii Ivan
+ */
+
+import java.util.Random;
+
+public class TaskA {
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[6][4];
+        fillRandom(matrix, -15, 15);
+    }
+private static final Random random = new Random();
+
+    private static void fillRandom(int[][] matrix, int min, int max) {
+    for (int[]row: matrix){
+        for (int i = 0; i < row.length; i++) {
+            row[i]=min+ random.nextInt(max-min+1);
+
+        }
+    }
+    }
+}
