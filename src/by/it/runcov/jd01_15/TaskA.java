2037cb0e237d07d7950a8bd3bbde8fef8bc53b66
user <Lobzik@.mail.com>
2021-04-02 12:31:08 +0300

    jd01_15

---
+++ src/by/it/runcov/jd01_15/TaskA.java
@@ -0,0 +1,16 @@
+package by.it.runcov.jd01_15;
+
+public class TaskA {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[6][4];
+        fillRandom(matrix, -15, 15);
+    }
+
+    private static void fillRandom(int[][] matrix, int min, int max) {
+        for(int[] row :matrix){
+            for (int i = 0; i < row.length; i++) {
+            }
+        }
+    }
+}
