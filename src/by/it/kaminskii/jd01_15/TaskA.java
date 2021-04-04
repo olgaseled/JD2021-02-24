c6697ea3f57be3d43587a92b819d51e954eb2633
vanek323 <ivankaminskii323@gmail.com>
2021-04-04 13:29:11 +0300

    jd01_05

--- src/by/it/kaminskii/jd01_15/TaskA.java
+++ src/by/it/kaminskii/jd01_15/TaskA.java
@@ -8,15 +8,14 @@ public class TaskA {
 
         int[][] matrix = new int[6][4];
         fillRandom(matrix, -15, 15);
+
+//        try() {
+//            
+//        }
     }
-private static final Random random = new Random();
+private static final Random random = new Random(123123123L);
 
     private static void fillRandom(int[][] matrix, int min, int max) {
-    for (int[]row: matrix){
-        for (int i = 0; i < row.length; i++) {
-            row[i]=min+ random.nextInt(max-min+1);
-
-        }
-    }
+    for (int[]row: matrix) for (int i = 0; i < row.length; i++) row[i] = min + random.nextInt(max - min + 1);
     }
 }
