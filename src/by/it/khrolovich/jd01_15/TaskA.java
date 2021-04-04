e9e468b338810f88a28fbfbf289bf2a3550c2981
MarinaKhrolovich <maniuneshka@gmail.com>
2021-04-04 20:59:45 +0300

    JD01_15 finished TaskA

--- src/by/it/khrolovich/jd01_15/TaskA.java
+++ src/by/it/khrolovich/jd01_15/TaskA.java
@@ -8,33 +8,46 @@ import java.nio.file.Paths;
 import java.util.Random;
 import java.util.stream.Stream;
 
-public class TaskA{
+public class TaskA {
     public static void main(String[] args) throws IOException {
         int[][] matrix = new int[6][4];
-        fillRandom(matrix,-15,15);
-        String fileName = FileNameHelper.getFilePath("matrix.txt",TaskA.class);
+        fillRandom(matrix, -15, 15);
+        String fileName = FileNameHelper.getFilePath("matrix.txt", TaskA.class);
+        saveToFile(matrix, fileName);
+        // Files.lines(Paths.get(fileName))//Stream<String>
+        printToConsoleFromFile(fileName);
+    }
 
-        try(PrintWriter out = new PrintWriter(fileName)){
+    private static void printToConsoleFromFile(String fileName) {
+        try {
+            Files.lines(Paths.get(fileName))
+                    .forEach(System.out::println);//readLine() все строки
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void saveToFile(int[][] matrix, String fileName) {
+        try (PrintWriter out = new PrintWriter(fileName)) {
             for (int[] row : matrix) {
                 for (int element : row) {
-                    System.out.println(element+" ");//?
+                    out.printf("%3d ", element);
                 }
-
+                out.println("");
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        Stream<String> line = Files.lines(Paths.get(fileName));
-
     }
 
-    private static  final Random random = new Random();
+    private static final Random random = new Random();
 
     private static void fillRandom(int[][] matrix, int min, int max) {
 
         for (int[] row : matrix) {
-
+            for (int i = 0; i < row.length; i++) {
+                row[i] = min + random.nextInt(max - min + 1);
+            }
         }
     }
 
