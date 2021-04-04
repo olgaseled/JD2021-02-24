814098365dde199514d74a2b64338cb6994e4b36
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-04 15:36:25 +0300

    JD01-15

--- src/by/it/levchuk/levchuk/jd01_15/FileNameHelper.java
+++ src/by/it/levchuk/levchuk/jd01_15/FileNameHelper.java
@@ -3,5 +3,22 @@ package by.it.levchuk.levchuk.jd01_15;
 /*crash-noobik
 Liauchuk Aliaksandr*/
 
+import java.io.File;
+
+import static by.it.levchuk.levchuk.jd01_14.TaskA.USER_DIR;
+import static java.awt.AlphaComposite.SRC;
+
+
+
 public class FileNameHelper {
+
+    static String getFilePath(String filename, Class<?> currentStructure) {
+        String rootProject = System.getProperty(USER_DIR);
+        String src = rootProject + File.separator + SRC + File.separator;
+        String relatedDir = currentStructure
+                .getName()
+                .replace(currentStructure.getSimpleName(), "")
+                .replace(".", File.separator);
+        return src + relatedDir + filename;
+    }
 }
