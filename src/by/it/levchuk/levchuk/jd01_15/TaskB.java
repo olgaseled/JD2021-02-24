c7eabdadcc0a9c9c0f98dc126c8eeb37498600fe
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-05 00:39:54 +0300

    JD01-15 homework

--- src/by/it/levchuk/levchuk/jd01_15/TaskB.java
+++ src/by/it/levchuk/levchuk/jd01_15/TaskB.java
@@ -1,9 +1,22 @@
 package by.it.levchuk.levchuk.jd01_15;
+
 /*crash-noobik
 Liauchuk Aliaksandr*/
 
+/**
+ * the file should have
+ * 2 single-line and 2 multi-line comments,
+ * and javaDoc
+ */
+
 public class TaskB {
-    public static void main(String[] args) {
+
+    public static final String JAVA_TASK = "TaskB.java";
+    public static final String TXT_TASK = "TaskB.txt";
+
+    public static void main(String[] args) { //start of the program execution
+        String roadToJava = FileNameHelper.getFilePath(JAVA_TASK, TaskB.class);
+        String roadToTxt = FileNameHelper.getFilePath(TXT_TASK, TaskB.class);
 
     }
 }
