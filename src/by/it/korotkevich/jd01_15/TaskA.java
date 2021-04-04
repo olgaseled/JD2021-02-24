ebd2430bc16365627d9d3ee5cc8e1a43e7fe813e
Korotkevich Alexey <lyoshakorotkevich0909@gmail.com>
2021-04-04 20:52:53 +0300

    jd01_14 B + jd01_15 B

--- src/by/it/korotkevich/jd01_15/TaskA.java
+++ src/by/it/korotkevich/jd01_15/TaskA.java
@@ -1,11 +1,43 @@
 package by.it.korotkevich.jd01_15;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
 
 public class TaskA {
+
+    public static final String MATRIX_TXT = "matrix.txt";
+
     public static void main(String[] args) {
         int[][] matrix = new int[6][4];
         fillRandom(matrix, -15, 15);
+        String fileName = Helper.getFilePath(MATRIX_TXT, TaskA.class);
+        saveTxtMatrix(matrix, fileName);
+        showFileInConsole(fileName);
+    }
+
+    private static void saveTxtMatrix(int[][] matrix, String fileName) {
+        try (PrintWriter out = new PrintWriter(fileName)){
+            for (int[] row : matrix) {
+                for (int element : row) {
+                    out.printf("%3d ", element);
+                }
+                out.println();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void showFileInConsole(String fileName) {
+        try {
+            Files.lines(Paths.get(fileName)).forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static final Random random = new Random();
