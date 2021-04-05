25db0c9c79390b50ef03996dbd10594eb5b500eb
MarinaKhrolovich <maniuneshka@gmail.com>
2021-04-05 12:30:47 +0300

    JD02_01 classwork

--- src/by/it/khrolovich/jd02_01/ICustomer.java
+++ src/by/it/khrolovich/jd02_01/ICustomer.java
@@ -1,6 +1,6 @@
 package by.it.khrolovich.jd02_01;
 
-interface IBuyer {
+interface ICustomer {
     void enterToMarket(); //вошел в магазин (мгновенно)
     void chooseGoods(); //выбрал товар (от 0,5 до 2 секунд)
     void goOut(); //отправился на выход(мгновенно)
