edb38fbf4cfcb344a7bd7c4fc52429bb5eefe894
MaksimovaNatalya <ps_nat98@mail.ru>
2021-04-05 17:35:03 +0300

    jd01_15 (A)

--- src/by/it/maksimova/jd01_15/TaskA.java
+++ src/by/it/maksimova/jd01_15/TaskA.java
@@ -1,10 +1,10 @@
 package by.it.maksimova.jd01_15;
 
-import by.it._classwork_.jd01_15.FileNameHelper;
-
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class TaskA {
@@ -15,17 +15,23 @@ public class TaskA {
     public static final String USER_DIR = "user.dir";
     public static final String SRC = "src";
     public static final String MATRIX_TXT = "matrix.txt";
-    String filename = dir(TaskA.class)+MATRIX_TXT;
 
-    private static String dir(Class<?> cl) {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[6][4];
+        generateMatrix(matrix);
+        String filename = getPath(TaskA.class) + MATRIX_TXT;
+        writeMatrix(matrix, filename);
+        showMatrix(filename);
+    }
+
+    private static String getPath(Class<?> cl) {
         String path = System.getProperty(USER_DIR) + File.separator + SRC + File.separator;
         String clDir = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
         return path + clDir;
     }
 
-    public static void main(String[] args) {
-        int[][] matrix = new int[6][4];
-        //generate matrix
+    private static void generateMatrix(int[][] matrix) {
         do {
             for (int[] row : matrix) {
                 for (int i = 0; i < row.length; i++) {
@@ -33,26 +39,27 @@ public class TaskA {
                 }
             }
         } while (matrix.toString().contains("15") && matrix.toString().contains("-15"));
+    }
 
-//writeMatrix
-try (PrintWriter printWriter = new PrintWriter(dir(TaskA.class)+ MATRIX_TXT)) {
-    for (int[] row : matrix) {
-        for (int element : row) {
-            printWriter.printf("%3d", element);
-            System.out.println();
+    private static void writeMatrix(int[][] matrix, String filename) {
+        try (PrintWriter printWriter = new PrintWriter(filename)) {
+            for (int[] row : matrix) {
+                for (int element : row) {
+                    printWriter.printf("%3d ", element);
+                }
+                printWriter.println();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
-} catch (FileNotFoundException e) {
-    throw new RuntimeException(e);
-}
 
+    private static void showMatrix(String filename) {
         try {
-            Files.lines(Paths.get(dir(TaskA.class)+MATRIX_TXT))
-                    .forEach(System.out::println);
+            Files.lines(Paths.get(filename))
+                    .forEach(x -> System.out.println(x));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 }
---
+++ src/by/it/maksimova/jd02_01/Customer.java
@@ -0,0 +1,4 @@
+package by.it.maksimova.jd02_01;
+
+public class Customer {
+}
