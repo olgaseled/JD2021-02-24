316df36f660dd9b7206bb940c3136ddf7faca9ee
MarinaKhrolovich <maniuneshka@gmail.com>
2021-04-05 14:48:09 +0300

    JD01_14 some changes JD01_15 finished with new methods

--- src/by/it/khrolovich/jd01_15/TaskB.java
+++ src/by/it/khrolovich/jd01_15/TaskB.java
@@ -6,15 +6,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 enum states {
-    one(),//one->one()просто текст, значит идем до слэы
-    two(),//после слэш текст значит опять ищем слэш
+    one(),//просто текст, значит идем до слэш
+    two(),//после слэш текст, значит опять ищем слэш
     tree(),//после слэш ещё один слэш значит ищем перевод коретки
-    four(),//после слэш *, значит идем до */
-    five();//после * опять *, значит идем до */
+    four();//после слэш *, значит идем до */
+    //five();//после * опять *, значит идем до */
     private String currentState;
 
     private String gerCurrentState() {
@@ -41,24 +40,30 @@ public class TaskB {
         String fileName = FileNameHelper.getFilePath("TaskB.java", TaskA.class);
         String fileNameTXT = FileNameHelper.getFilePath("TaskB.txt", TaskA.class);
         System.out.println(fileName);
-        Stream<String> lines = null;
         StringBuilder sb = new StringBuilder();
         //sb.append("привет").append('\n').append("//привет").append('\n').append("/*привет*/");
 
-        try {
-            Files.lines(Paths.get(fileName)).forEach(s -> sb.append(s).append('\n'));
-        } catch (IOException e) {
+        saveToString(fileName, sb);
+        deleteComment(sb);
+        System.out.println(sb.toString());
+        printToFile(fileNameTXT, sb);
+    }
+
+    private static void printToFile(String fileNameTXT, StringBuilder sb) {
+        try(PrintWriter pw =new PrintWriter(fileNameTXT)){
+            pw.write(sb.toString());
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private static void deleteComment(StringBuilder sb) {
         char[] chars = new char[sb.length()];
-
         sb.getChars(0, sb.length(), chars, 0);
         String s = String.copyValueOf(chars);
         int start = 0;
         int end = 0;
         int position = 0;
-        int countOfDel = 0;
         boolean isCommentSimple = false;
         boolean isCommentMulti = false;
         int currentInd = 0;
@@ -66,9 +71,9 @@ public class TaskB {
             switch (aChar) {
                 case '/':
                     if (start == 0) {
-                        start = currentInd;//подозреваем комментарий
+                        start = currentInd;//may be find simple comment
                     } else {
-                        if (start == currentInd - 1) {//нашли комментарий
+                        if (start == currentInd - 1) {//find simple comment
                             isCommentSimple = true;
 
                         }else{
@@ -79,40 +84,36 @@ public class TaskB {
                     }
                     break;
                 case '*':
-                    if (start == currentInd - 1) {//нашли комментарий
+                    if (start == currentInd - 1) {//find multiply comment
                         isCommentMulti = true;
                     }
                     break;
                 case '\n':
-                    if (isCommentSimple) {
+                    if (isCommentSimple) {//find the end of  simple comment
                         end = currentInd-1;
                     }
                     break;
                 default: if (start == currentInd - 1){start =0;}
-                    ;
             }
+            //delete comment if find it
             if (start != 0 && end != 0) {
                 sb.replace(start-position, end-position+1, "");
-                countOfDel++;
                 isCommentSimple = false;
                 isCommentMulti = false;
-                //currentInd = start;
-                if(countOfDel==1) {
-                    //System.out.println(countOfDel);
-                };
                 position =position+ (end-start+1);
                 start = 0;
                 end = 0;
-                //break;
             }
             currentInd++;
 
         }
-        System.out.println(sb.toString());
-        try(PrintWriter pw =new PrintWriter(fileNameTXT)){
-            pw.write(sb.toString());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    }
+
+    private static void saveToString(String fileName, StringBuilder sb) {
+        try {
+            Files.lines(Paths.get(fileName)).forEach(s -> sb.append(s).append('\n'));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
