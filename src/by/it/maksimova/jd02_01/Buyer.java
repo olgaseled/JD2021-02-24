e592b2a5d180202af1eb78596b9381017823880e
MaksimovaNatalya <ps_nat98@mail.ru>
2021-04-06 20:16:12 +0300

    jd02_01

---
+++ src/by/it/maksimova/jd02_01/Buyer.java
@@ -0,0 +1,20 @@
+package by.it.maksimova.jd02_01;
+
+public class Buyer implements IBuyer{
+
+
+    @Override
+    public void enterToMarket() {
+
+    }
+
+    @Override
+    public void chooseGoods() {
+
+    }
+
+    @Override
+    public void goOut() {
+
+    }
+}
