103f3c05975c1385f08efd13342e67f6250584b3
seledtsova <olgaseledtsova9@yandex.by>
2021-04-06 11:22:56 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2021-02-24
     Conflicts:
    	src/by/it/seledtsova/jd01_09/ConsoleRunner.java
    	src/by/it/seledtsova/jd01_09/Matrix.java
    	src/by/it/seledtsova/jd01_09/Operation.java
    	src/by/it/seledtsova/jd01_09/Parser.java
    	src/by/it/seledtsova/jd01_09/Patterns.java
    	src/by/it/seledtsova/jd01_09/Printer.java
    	src/by/it/seledtsova/jd01_09/Scalar.java
    	src/by/it/seledtsova/jd01_09/Test_jd01_09.java
    	src/by/it/seledtsova/jd01_09/Var.java
    	src/by/it/seledtsova/jd01_09/VarCreator.java
    	src/by/it/seledtsova/jd01_09/Vector.java

--- src/by/it/seledtsova/jd02_01/Store.java
+++ src/by/it/seledtsova/jd02_01/Store.java
@@ -4,8 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Store {
+
     public static void main(String[] args) {
+        int countBuyers=0; // счетчик покупателей
         List <Buyer> buyersList=new ArrayList<>();
         System.out.println("The store is opened");
+
+
+        }
     }
-}
+
+/*
+int numberCustomer = 0;
+        for (int time = 0; time < Config.FINAL_TIME; time++) {
+            int count = Util.getRandom(2);
+            for (int i = 0; i < count; i++) {
+                Customer customer = new Customer(++numberCustomer);
+                customers.add(customer);
+                customer.start();
+            }
+            Util.sleep(1000);
+        }
+        for (Customer customer : customers) {
+            try {
+                customer.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Store closed");
+    }
+ */
