21bc59390700ae8e8713d064be903dfbf75cb0ac
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-06 23:21:25 +0300

    JD02-01 homework

--- src/by/it/levchuk/levchuk/jd02_01/PriceList.java
+++ src/by/it/levchuk/levchuk/jd02_01/PriceList.java
@@ -10,7 +10,16 @@ public class PriceList {
     HashMap<String,Double> priceList = new HashMap<>();
 
     public void setPriceList(HashMap<String, Double> priceList) {
+
         priceList.put("Хлеб", 2.15);
+        priceList.put("Молоко", 2.15);
+        priceList.put("Масло", 2.15);
+        priceList.put("Мясо", 2.15);
+        priceList.put("Овощи", 2.15);
+        priceList.put("Фрукты", 2.15);
+        priceList.put("Напитки", 2.15);
+        priceList.put("Алкоголь", 2.15);
+        priceList.put("Сигареты", 2.15);
 
         this.priceList = priceList;
     }
