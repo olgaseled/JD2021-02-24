99c42affb814a18f6e028276a9775f2b2f03b25c
seledtsova <olgaseledtsova9@yandex.by>
2021-04-07 16:06:27 +0300

    h

--- src/by/it/seledtsova/jd01_04/TaskB.java
+++ src/by/it/seledtsova/jd01_04/TaskB.java
@@ -12,57 +12,50 @@ import java.util.Scanner;
 
 public class TaskB {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-
         int n = scanner.nextInt(); // количество людей 3
-
         String[] lastNames = new String[n];
         for (int i = 0; i < lastNames.length; i++) {
             lastNames[i] = scanner.next(); // фамилии сотрудников
         }
-        int salaryArr[][]; // создала массив з/п сотрудников с фамилиями
-        salaryArr = new int[n][4];
+        int salaryArr[][] = new int[n][4]; // создала массив з/п сотрудников с фамилиями
         for (int i = 0; i < salaryArr.length; i++) {
             System.out.printf("Введите зарплату для %s\n", lastNames[i]);//вводим з/п сотрудников
             for (int j = 0; j < salaryArr[0].length; j++) {
                 salaryArr[i][j] = scanner.nextInt();
-
             }
-            System.out.println(Arrays.deepToString(salaryArr));
-        }
-
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%-10s%n", "Фамилия", "Квартал1", "Квартал2", "Квартал3", "Квартал4", "ИТОГО");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%-10s%n", "Иванов", "1", "2", "3", "4", "10");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%-10s%n", "Петров", "5", "6", "7", "8", "26", "42");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%-10s%n", "Сидоров", "9", "10", "11", "12", "42");
-        System.out.println();
+            }
 
 
-        double total = 0;
-
+        int totalSalary = 0;
         for (int i = 0; i < salaryArr.length; ++i) {
             double sumRow = 0;
             for (int j = 0; j < salaryArr[0].length; j++) {
                 sumRow += salaryArr[i][j];
-                total += salaryArr[i][j];
+                totalSalary = totalSalary + salaryArr[i][j];
             }
-                System.out.println("Общая сумма всех выплат=" + total);
-                System.out.println();
-
-            double allSalaryKv = 0;
-            for (int j = 0; j < salaryArr[0].length; j++) {
-                double averageSalary = 0;
-                for (int k = 0; k < n; k++) {
-                    averageSalary = averageSalary + salaryArr[i][j];
-                }
-                allSalaryKv = allSalaryKv + averageSalary / n;
-            }
-            System.out.printf("средняя: %.4f", allSalaryKv / 4);
-
-
         }
-    }
-}
+            double averageSalary = 0;
+            averageSalary = (double) totalSalary / (salaryArr.length * salaryArr[0].length);
+
+            System.out.printf("%-8s%-10s%-10s%-10s%-10s%-10s%n", "Фамилия", "Квартал1", "Квартал2", "Квартал3", "Квартал4", "ИТОГО");
+            System.out.println("_______________________________________________________");
+            for (int i = 0; i < salaryArr.length; i++) {
+                System.out.printf("%-8s%-10s%-10s%-10s%-10s%-10s%n",
+                        lastNames[i], salaryArr[i][0], salaryArr[i][1], salaryArr[i][2], salaryArr[i][3],
+                        salaryArr[i][0] + salaryArr[i][1] + salaryArr[i][2] + salaryArr[i][3]);
+            }
+            System.out.println("________________________________________________________");
+            System.out.printf("%-10s %-4s", "ИТОГО", totalSalary);
+            System.out.println();
+            System.out.printf("%-10s %-4s", "Средняя", averageSalary);
+
+            }
+        }
+
+
+
 
 
 
