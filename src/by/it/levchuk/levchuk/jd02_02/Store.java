55008f12ba45abc724547003741be147dbfafe41
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-08 23:57:04 +0300

    JD02-02 Homework

--- src/by/it/levchuk/levchuk/jd02_02/Store.java
+++ src/by/it/levchuk/levchuk/jd02_02/Store.java
@@ -3,32 +3,35 @@ package by.it.levchuk.levchuk.jd02_02;
 /*crash-noobik
 Liauchuk Aliaksandr*/
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 
 public class Store {
 
     public static void main(String[] args) {
 
-        List<Buyer> buyers=new ArrayList<>(Сoefficients.VISIT_TIME);
+        List<Thread> threads = new ArrayList<>(Сoefficients.VISIT_TIME);
+        for (int i = 0; i <= 2; i++) {
+            Cashier cashier = new Cashier(i);
+            Thread thread = new Thread(cashier, cashier.toString());
+            threads.add(thread);
+            thread.start();
+        }
 
         System.out.println("МАГАЗИН ОТКРЫЛСЯ");
         int numberBuyer = 0;
-        Queue<Buyer> queue = new ArrayDeque<>();
         for (int time = 0; time < Сoefficients.VISIT_TIME; time++) {
             int count = Tools.getRandom();
             for (int i = 0; i < count; i++) {
                 Buyer buyer = new Buyer(++numberBuyer);
-                queue.add(buyer);
+                threads.add(buyer);
                 buyer.start();
             }
             Tools.sleep(1000);
         }
-        for (Buyer buyer : queue) {
+        for (Thread thread : threads) {
             try {
-                buyer.join();
+                thread.join();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
