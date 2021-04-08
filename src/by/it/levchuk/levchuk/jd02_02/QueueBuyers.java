eee9756b0d170ff35d3c70a7c4d1947e6c26e81a
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-08 22:59:12 +0300

    JD02-02 Homework

--- src/by/it/levchuk/levchuk/jd02_02/QueueBuyers.java
+++ src/by/it/levchuk/levchuk/jd02_02/QueueBuyers.java
@@ -9,7 +9,7 @@ public class QueueBuyers {
 
     private static final Object MONITOR_QUEUE = new Object();
 
-    private static Deque<Buyer> buyers = new LinkedList<>();
+    private static final Deque<Buyer> buyers = new LinkedList<>();
 
     static void add(Buyer buyer){
         synchronized (MONITOR_QUEUE){
