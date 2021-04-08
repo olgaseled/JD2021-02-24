37c255e0ca4cf5dc110c8907997a3eb729fa10b6
Maxim_Savchenko <ya.max11122@yandex.by>
2021-04-09 00:59:29 +0300

    rr

---
+++ src/by/it/savchenko/jd02_02/Customer.java
@@ -0,0 +1,72 @@
+package by.it.savchenko.jd02_02;
+
+class Customer extends Thread implements ICustomer {
+
+    private final Object MONITOR;
+
+    private boolean waiting = false;
+
+    public Object getMONITOR() {
+        return MONITOR;
+    }
+
+    public void setWaiting(boolean waiting) {
+        this.waiting = waiting;
+    }
+
+    Customer(int number) {
+        super("Customer #" + number + " ");
+        MONITOR = this;
+        Manager.newCustomer();
+
+    }
+
+    @Override
+    public void run() {
+        enterToMarket();
+        chooseGoods();
+        goToQueue();
+        goOut();
+        Manager.completeCustomer();
+    }
+
+    @Override
+    public void enterToMarket() {
+        System.out.println(this + "goes to the store");
+    }
+
+    @Override
+    public void chooseGoods() {
+        System.out.println(this + "started  choose goods");
+        int timeout = Util.getRandom(500, 2000);
+        Util.sleep(timeout);
+        System.out.println(this + "finished  choose goods");
+    }
+
+    @Override
+    public void goToQueue() {
+        synchronized (MONITOR) {
+            QueueCustomers.add(this);
+            waiting = true;
+            while (waiting) {
+                try {
+                    MONITOR.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
+    @Override
+    public void goOut() {
+        System.out.println(this + "leaves the store");
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+}
