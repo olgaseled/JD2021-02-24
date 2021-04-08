a6c214e7daacb918147260b8768155f4343da6ac
Korotkevich Alexey <lyoshakorotkevich0909@gmail.com>
2021-04-08 17:06:35 +0300

    jd02_01 B исправлено и переделано

--- src/by/it/korotkevich/jd02_01/Basket.java
+++ src/by/it/korotkevich/jd02_01/Basket.java
@@ -1,5 +1,19 @@
 package by.it.korotkevich.jd02_01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Basket {
     //перенести всё что связано с корзиной из класса клиент
+
+
+    static void putGoodsToBasket(Customer customerName, int goodsAmount) {
+        List<Good> basketContents = new ArrayList<>();
+        for (int i = 0; i < goodsAmount; i++) {
+            Util.sleep(Util.getRandom(500, 2000));
+            Good oneGood = GoodsList.getAGood();
+            basketContents.add(oneGood);
+            System.out.println(customerName + "puts " + oneGood.toLowerCase(oneGood) + " inside his basket.");
+        }
+    }
 }
