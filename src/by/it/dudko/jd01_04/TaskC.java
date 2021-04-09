8b584738d4dc056e92062fd7dcd9c95d1d69dad1
alexey <leva@mail.com>
2021-04-09 23:38:35 +0300

    feat: [jd01_04] TaskC1 fix mergeSort contract

--- src/by/it/dudko/jd01_04/TaskC.java
+++ src/by/it/dudko/jd01_04/TaskC.java
@@ -27,54 +27,41 @@ public class TaskC {
         System.out.printf("Index of first element=%d\nIndex of last element=%d", indexOfFirst, indexOfLast);
     }
 
-    public static double[] mergeSort(double[] list) {
-        // If list is empty; no need to do anything
-        if (list.length <= 1) {
-            return list;
+    static void mergeSort(double[] arr) {
+        if (arr.length > 1) {
+            int middle = arr.length / 2;
+            double[] part1 = new double[middle];
+            double[] part2 = new double[arr.length - part1.length];
+            System.arraycopy(arr, 0, part1, 0, part1.length);
+            System.arraycopy(arr, part1.length, part2, 0, part2.length);
+            // sort both halves recursively
+            mergeSort(part1);
+            mergeSort(part2);
+            merge(part1, part2, arr);
         }
-
-        // Create 2 lists to hold 1st half and 2nd half of original list.
-        double[] first = new double[list.length / 2];
-        double[] second = new double[list.length - first.length];
-        // Split the array in half and populate above lists.
-        System.arraycopy(list, 0, first, 0, first.length);
-        System.arraycopy(list, first.length, second, 0, second.length);
-
-        // Sort each half recursively
-        mergeSort(first);
-        mergeSort(second);
-
-        // Merge both halves together, overwriting original array
-        merge(first, second, list);
-        return list;
     }
 
+//    private static void mergeSort(double[] arr, int left, int right) {
+//
+//    }
 
-    private static void merge(double[] first, double[] second, double[] result) {
-        // Index Position in first array - starting with first element
-        int iFirst = 0;
+    private static void merge(double[] part1, double[] part2, double[] result) {
 
-        // Index Position in second array - starting with first element
-        int iSecond = 0;
-
-        // Index Position in merged array - starting with first position
-        int iMerged = 0;
-
-        // Compare elements at iFirst and iSecond,
-        // and move smaller element at iMerged
-        while (iFirst < first.length && iSecond < second.length) {
-            if (first[iFirst] < second[iSecond]) {
-                result[iMerged] = first[iFirst];
-                iFirst++;
+        int cursorPart1 = 0;
+        int cursorPart2 = 0;
+        int cursorMerged = 0;
+        while (cursorPart1 < part1.length && cursorPart2 < part2.length) {
+            if (part1[cursorPart1] < part2[cursorPart2]) {
+                result[cursorMerged] = part1[cursorPart1];
+                cursorPart1++;
             } else {
-                result[iMerged] = second[iSecond];
-                iSecond++;
+                result[cursorMerged] = part2[cursorPart2];
+                cursorPart2++;
             }
-            iMerged++;
+            cursorMerged++;
         }
-        // copy remaining elements from both halves - each half will have already sorted
-        // elements
-        System.arraycopy(first, iFirst, result, iMerged, first.length - iFirst);
-        System.arraycopy(second, iSecond, result, iMerged, second.length - iSecond);
+        // copy remaining elements from both halves
+        System.arraycopy(part1, cursorPart1, result, cursorMerged, part1.length - cursorPart1);
+        System.arraycopy(part2, cursorPart2, result, cursorMerged, part2.length - cursorPart2);
     }
 }
