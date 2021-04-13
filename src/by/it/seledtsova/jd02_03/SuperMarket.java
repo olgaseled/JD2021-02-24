3056ce113c25a6c2a75b3593543ec38f9fcaa8a1
seledtsova <olgaseledtsova9@yandex.by>
2021-04-13 20:05:02 +0300

    jd02_01 B jd02_02 jd 02_03

--- src/by/it/seledtsova/jd02_03/SuperMarket.java
+++ src/by/it/seledtsova/jd02_03/SuperMarket.java
@@ -11,38 +11,37 @@ public class SuperMarket extends ProductList {
     public static ProductList productList;
 
 
-      public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
-          System.out.println("The store is opened");
+        System.out.println("The store is opened");
 
-          ExecutorService threadCashier =Executors.newFixedThreadPool(5);// будет всего 5 одновременно работающих кассиров
-          for (int i = 1; i <=2 ; i++) { // запускаем от 1 до 2 кассиров
-              Cashier cashier=new Cashier(i); // создали кассира нового
-              threadCashier.execute(cashier); // запустили кассира в пул потока
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);// будет всего 5 одновременно работающих кассиров
+        for (int i = 1; i <= 2; i++) { // запускаем от 1 до 2 кассиров
+            Cashier cashier = new Cashier(i); // создали кассира нового
+            threadPool.execute(cashier); // запустили кассира в пул потока
 
+        }
+
+        int countBuyers = 0; // счетчик покупателей
+
+        while (Manager.marketIsOpened()) {
+            int count = Util.getRandom(2);
+            for (int i = 0; i < count && Manager.marketIsOpened(); i++) {
+                Buyer buyer = new Buyer(++countBuyers);
+                threadPool.execute(buyer); // запустили покупателейв пул потоков
             }
-          threadCashier.shutdown();// остановили поток кассиров
-
-
-          int countBuyers = 0; // счетчик покупателей
-
-          while (Manager.marketIsOpened()) {
-              int count = Util.getRandom(2);
-              for (int i = 0; i < count && Manager.marketIsOpened(); i++) {
-                  Buyer buyer = new Buyer(++countBuyers);
-                  buyer.start();
-              }
             Util.sleep(1000);
-              }
-          while (true){ // пул кассиров должен перейти в состояние isTerminated, чтобы закрыть магазин
-              try {
-        if (threadCashier.awaitTermination(10, TimeUnit.DAYS)) { // если мы дождались ( прошло менее 10 дней) , что кассиры закончили
-            break; // то наш бесконечный цикл закончится раньше 10 дней
-          }
-              } catch (InterruptedException e) {
-                throw new RuntimeException  (e);
-              }
-          }
+        }
+        threadPool.shutdown();// остановили поток кассиров и покупателей
+        while (true) { // пулы кассиров и покупателей должны перейти в состояние isTerminated, чтобы закрыть магазин
+            try {
+                if (threadPool.awaitTermination(10, TimeUnit.DAYS)) { // если мы дождались, что кассиры и покупатели закончили
+                    break; // то наш бесконечный цикл закончится раньше 10 дней
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         System.out.println("The store  has closed"); // все потоки завершены и мазаг закрыт
 
     }
