1a4a6a4983a35ec88c4e3dc930841d201aa569d3
Korotkevich Alexey <lyoshakorotkevich0909@gmail.com>
2021-04-15 15:33:59 +0300

    jd02_02 задание B

--- src/by/it/korotkevich/calc/Parser.java
+++ src/by/it/korotkevich/calc/Parser.java
@@ -1,5 +1,7 @@
 package by.it.korotkevich.calc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,36 +9,71 @@ import java.util.regex.Pattern;
 class Parser {
 
     Var evaluate(String expression) throws CalcException {
-        expression = expression.replaceAll("\\s+", "");
-        String[] parts = expression.split(Patterns.OPERATION, 2);
-        Var leftVar = VarCreator.build(parts[0]);
-        if (parts.length < 2) {
-            return leftVar;
+        ArrayList<String> operands = new ArrayList<>(Arrays.asList(expression.split(Patterns.OPERATION)));
+        Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(expression);
+        ArrayList<String> operations = new ArrayList<>();
+        while (matcher.find()) {
+            operations.add(matcher.group());
         }
-        Var rightVar = VarCreator.build(parts[1]);
-        if (expression.contains("=")) {
-            return Var.saveVar(parts[0], rightVar);
+        while (operations.size() > 0) {
+            int index = getIndex(operations);
+            if (operations.contains("printVar")){
+                Map<String, Var> vars = Var.vars;
+                Var.printVar(vars);
+                operations.clear();
+                return null;
+            }
+            String operation = operations.remove(index);
+            String leftOperand = operands.remove(index);
+            String rightOperand = operands.remove(index);
+            Var result = oneOperation(leftOperand, operation, rightOperand);
+            operands.add(index, result.toString());
         }
-        Pattern patternOperation = Pattern.compile(Patterns.OPERATION);
-        Matcher matcherOperation = patternOperation.matcher(expression);
-        if (matcherOperation.find()) {
-            String operation = matcherOperation.group();
-            switch (operation) {
-                case "+":
-                    return leftVar.add(rightVar);
-                case "-":
-                    return leftVar.sub(rightVar);
-                case "*":
-                    return leftVar.mul(rightVar);
-                case "/":
-                    return leftVar.div(rightVar);
-                case "printVar":
-                    Map<String, Var> vars = Var.vars;
-                    Var.printVar(vars);
+        return VarCreator.build(operands.get(0));
+    }
+
+    private Var oneOperation(String leftOperand, String operation, String rightOperand) throws CalcException {
+        Var rightVar = VarCreator.build(rightOperand);
+        if (operation.equals("=")){
+            return Var.saveVar(leftOperand, rightVar);
+        }
+        Var leftVar = VarCreator.build(leftOperand);
+
+        switch (operation){
+            case "+":
+                return leftVar.add(rightVar);
+            case "-":
+                return leftVar.sub(rightVar);
+            case "*":
+                return leftVar.mul(rightVar);
+            case "/":
+                return leftVar.div(rightVar);
+            case "printVar":
+                Map<String, Var> vars = Var.vars;
+                Var.printVar(vars);
+        }
+        throw new CalcException("Impossible operation");
+    }
+
+    private static final Map<String, Integer> pr = Map.of(
+            "=", 0,
+            "+", 1,
+            "-", 1,
+            "*", 2,
+            "/", 2,
+            "printVar", 3
+    );
+
+    private int getIndex(ArrayList<String> operations) {
+        int index = -1;
+        int best = -1;
+        for (int i = 0; i < operations.size(); i++) {
+            int currentPr = pr.get(operations.get(i));
+            if (currentPr > best) {
+                index = i;
+                best = currentPr;
             }
         }
-
-
-        return null; //TODO stub
+        return index;
     }
 }
--- src/by/it/korotkevich/calc/Patterns.java
+++ src/by/it/korotkevich/calc/Patterns.java
@@ -2,7 +2,7 @@ package by.it.korotkevich.calc;
 
 interface Patterns {
 
-    String OPERATION = "[-=+*/]|\\bprintVar\\b";
+    String OPERATION = "(?<=[^-+*/=,{])[-+*/=]|\\bprintVar\\b";
     String SCALAR = "-?[0-9]+\\.?[0-9]*";
     String VECTOR = "\\{((-?[0-9]+\\.?[0-9]*),?)+}";
     String MATRIX = "\\{(\\{((-?[0-9]+\\.?[0-9]*),?)+},?)+}";
@@ -13,6 +13,7 @@ interface Patterns {
     {{1.2,2},{8,3}}*{{1,2},{8,3}}
     {{1,2,3,4},{8,3,3}}*{{1,2,2},{8,3}}
     printVar
+    2*(2+(2/5))
      */
 
 }
