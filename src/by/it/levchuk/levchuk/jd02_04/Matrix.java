dce448834c15f6bd3981274908afb31aa72fd33f
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-17 20:16:56 +0300

    JD02-04_Calc test two method Homework

--- src/by/it/levchuk/levchuk/jd02_04/Matrix.java
+++ src/by/it/levchuk/levchuk/jd02_04/Matrix.java
@@ -8,6 +8,29 @@ public class Matrix extends Var {
 
     private final double[][] value;
 
+    @Override
+    public Var add(Var other) throws CalcException {
+        if (other instanceof Scalar){
+
+        }
+        return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) throws CalcException {
+        return super.sub(other);
+    }
+
+    @Override
+    public Var mul(Var other) throws CalcException {
+        return super.mul(other);
+    }
+
+    @Override
+    public Var div(Var other) throws CalcException {
+        return super.div(other);
+    }
+
     public Matrix(double[][] value) {
         this.value = new double[value.length][0];
         for (int i = 0; i < value.length; i++) {
