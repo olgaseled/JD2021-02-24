20908a24c2e1637c54cf6b523c38eb6af71b1490
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-18 20:25:00 +0300

    JD02-05 Homework

---
+++ src/by/it/levchuk/levchuk/jd02_05/Runner.java
@@ -0,0 +1,17 @@
+package by.it.levchuk.levchuk.jd02_05;
+
+/*crash-noobik
+Liauchuk Aliaksandr*/
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Locale("ru","RU");
+        String res = "by.it.levchuk.levchuk.jd02_05.resources.language";
+        ResourceBundle bundle = ResourceBundle.getBundle(res);
+        bundle.getString("messages.welcome");
+
+    }
+}
