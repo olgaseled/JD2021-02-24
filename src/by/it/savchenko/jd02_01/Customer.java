e9b16926cf44698b509686935bf44b29a8808d4b
Maxim_Savchenko <ya.max11122@yandex.by>
2021-04-19 01:09:20 +0300

    rr

--- src/by/it/savchenko/jd02_01/Customer.java
+++ src/by/it/savchenko/jd02_01/Customer.java
@@ -1,6 +1,6 @@
 package by.it.savchenko.jd02_01;
 
-class Customer extends Thread implements ICustomer {
+class Customer extends Thread implements ICustomer, IUseBasket {
 
     public Customer(int number) {
         super("Customer #" + number + " ");
@@ -11,11 +11,19 @@ class Customer extends Thread implements ICustomer {
         enterToMarket();
         chooseGoods();
         goOut();
+        takeBasket();
+        putGoodsToBasket();
     }
 
     @Override
     public void enterToMarket() {
         System.out.println(this + "goes to the store");
+    }
+    @Override
+    public void takeBasket() {
+        System.out.println(this + "took a Basket");
+        int timeout = Util.getRandom(500, 2000);
+        Util.sleep(timeout);
     }
 
     @Override
@@ -26,6 +34,7 @@ class Customer extends Thread implements ICustomer {
         System.out.println(this + "finished choose goods");
     }
 
+
     @Override
     public void goOut() {
         System.out.println(this + "leaves the store");
@@ -35,4 +44,22 @@ class Customer extends Thread implements ICustomer {
     public String toString() {
         return this.getName();
     }
-}
+
+
+
+    @Override
+    public void putGoodsToBasket()  {
+
+        int numberOfGoods = Util.getRandom(1, 4);
+        for (int i = 0; i < numberOfGoods; i++) {
+            String nameOfGood = ListProduct.getRandomProduct();
+            Double priceOfGood = ListProduct.basket.get(nameOfGood);
+            System.out.println(this + "put " + nameOfGood + " costing " + priceOfGood + " to the Basket");
+            int timeout = Util.getRandom(500, 2000);
+            Util.sleep(timeout);
+        }
+        System.out.println(this + "finished choosing goods");
+    }
+
+    }
+
--- src/by/it/savchenko/jd02_01/Store.java
+++ src/by/it/savchenko/jd02_01/Store.java
@@ -3,11 +3,11 @@ package by.it.savchenko.jd02_01;
 import java.util.ArrayList;
 import java.util.List;
 
-class Store {
+class Store extends ListProduct {
 
 
     public static void main(String[] args) {
-        List<Customer> customers = new ArrayList<>();
+        List<Customer> customers = new ArrayList<>(120);
         System.out.println("Store opened");
         int numberCustomer = 0;
         for (int time = 0; time < Config.FINAL_TIME; time++) {
