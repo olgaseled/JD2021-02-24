822ab4dea3b5bf25dd98cf3512b6c6dca607b904
Andrey <vtna1569@gmail.com>
2021-04-18 20:22:38 +0300

    JD02_04(B)

--- src/by/it/voitsekhovskiy/jd02_04/Scalar.java
+++ src/by/it/voitsekhovskiy/jd02_04/Scalar.java
@@ -8,6 +8,7 @@ class Scalar extends Var {
         if (other instanceof Scalar) {
             return new Scalar(this.value + ((Scalar) other).value);
         }
+        
         return other.add(this);
     }
 
