2c3baa0a9dff92b2786c2d55cc497dd0b378db7e
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-18 22:01:35 +0300

    JD02-05 Homework

--- src/by/it/levchuk/levchuk/jd02_05/txt/Messages.java
+++ src/by/it/levchuk/levchuk/jd02_05/txt/Messages.java
@@ -2,5 +2,5 @@ package by.it.levchuk.levchuk.jd02_05.txt;
 
 public interface Messages {
     String WELCOME="messages.welcome";
-    String QUESTION="Nice day, isn't it?";
+    String QUESTION="messages.question";
 }
--- src/by/it/levchuk/levchuk/jd02_05/txt/User.java
+++ src/by/it/levchuk/levchuk/jd02_05/txt/User.java
@@ -1,10 +1,10 @@
 package by.it.levchuk.levchuk.jd02_05.txt;
 
 public interface User {
-    String FIRST_NAME="Aleksandr";
-    String LAST_NAME="Levchuk";
-    String NICKNAME="crash-noobik";
-    String I_AM="I am a student of the course Java";
-    String TEACHER="Teacher Alexander Khmelov";
-    String LEVEL="I am junior";
+    String FIRST_NAME="user.firstname";
+    String LAST_NAME="user.lastname";
+    String NICKNAME="user.nickname";
+    String I_AM="user.i_am";
+    String TEACHER="user.teacher";
+    String LEVEL="user.level";
 }
