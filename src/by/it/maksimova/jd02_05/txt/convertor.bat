a1e26ebf5f19ab7be39caf12c55aaa1e32a7e2d7
MaksimovaNatalya <ps_nat98@mail.ru>
2021-04-19 21:17:12 +0300

    jd02_04

---
+++ src/by/it/maksimova/jd02_05/txt/convertor.bat
@@ -0,0 +1,4 @@
+set path=D:\Java\JDK8\bin
+native2ascii -encoding UTF-8 language.txt ..\resources\language.properties
+native2ascii -encoding UTF-8 language.txt ..\resources\language_ru_RU.properties
+native2ascii -encoding UTF-8 language.txt ..\resources\language_be_BY.properties
