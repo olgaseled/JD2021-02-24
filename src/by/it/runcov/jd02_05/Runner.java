38d23da6a8222a827b124be82a26de97de7368bf
TimofeyRuncov <timofey.runcov@mail.ru>
2021-04-19 21:11:24 +0300

    jd02_05

--- src/by/it/runcov/jd02_05/Runner.java
+++ src/by/it/runcov/jd02_05/Runner.java
@@ -1,19 +1,53 @@
 package by.it.runcov.jd02_05;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
-        Language lang = Language.INSTANCE;
+
+        Scanner scanner = new Scanner(System.in);
         Locale.setDefault(Locale.ENGLISH);
-        if (args.length == 2) {
-            lang.setLocale(new Locale(args[0], args[1]));
+        Language lang = Language.INSTANCE;
+        Date date = new Date();
+        String language = "";
+        String country = "";
+
+        switch (scanner.nextLine()) {
+
+            case "en":
+                language = "en";
+                country = "US";
+                break;
+
+            case "be":
+                language = "be";
+                country = "BY";
+                break;
+
+            case "ru":
+                language = "ru";
+                country = "RU";
+                break;
+
+
+            default:
+                if (args.length == 2) {
+                    language = args[0];
+                    country = args[1];
+                }
         }
+
+        lang.setLocale(new Locale(language, country));
+        DateFormat df = DateFormat.getDateInstance(DateFormat.LONG, new Locale(language, country));
 
 
         System.out.println(lang.get(Massages.WELCOME));
         System.out.println(lang.get(User.FIRST_NAME));
         System.out.println(lang.get(User.LAST_NAME));
         System.out.println(lang.get(Massages.QUESTION));
+        System.out.println(df.format(date));
     }
 }
