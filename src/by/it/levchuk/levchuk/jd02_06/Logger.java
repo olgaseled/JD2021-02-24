33cb6611bcbbfde3d04548c6b3907edf5a379b19
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-19 22:45:05 +0300

    JD02-06 Homework

--- src/by/it/levchuk/levchuk/jd02_06/Logger.java
+++ src/by/it/levchuk/levchuk/jd02_06/Logger.java
@@ -3,18 +3,40 @@ package by.it.levchuk.levchuk.jd02_06;
 /*crash-noobik
 Liauchuk Aliaksandr*/
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Logger {
-//    private static volatile Logger logger;
-//    private  Logger(){
-//    }
-//
-//    static Logger get(){
-//        Logger localLogger=logger;
-//        if (localLogger==null){
-//            synchronized (Logger.class){
-//                localLogger=logger;
-//                if (localLogger==null)
-//            }
-//        }
-//    }
+    private final File logFile;
+    private static volatile Logger instanceLogger;
+
+    private Logger() {
+        logFile = Util.getFile(Logger.class, "LogList.txt");
+    }
+
+    public static Logger getInstanceLogger() {
+        Logger localLogger = instanceLogger;
+        if (localLogger == null) {
+            synchronized (Logger.class) {
+                localLogger = instanceLogger;
+                if (localLogger == null) {
+                    localLogger = new Logger();
+                    instanceLogger = localLogger;
+                }
+            }
+        }
+        return localLogger;
+    }
+
+    synchronized void log(String logText) {
+        try (
+                PrintWriter logWriter = new PrintWriter(new FileWriter(logFile, true))
+        ) {
+            logWriter.println(logText);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
