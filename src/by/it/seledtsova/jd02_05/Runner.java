7b5c9a81a2ef135ed29ae60b84f7e3a913dbc6b4
seledtsova <olgaseledtsova9@yandex.by>
2021-04-19 16:25:54 +0300

    jd02_05 A

--- src/by/it/seledtsova/jd02_05/Runner.java
+++ src/by/it/seledtsova/jd02_05/Runner.java
@@ -4,28 +4,53 @@ package by.it.seledtsova.jd02_05;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Runner implements Data {
 
 
     public static void main(String[] args) {
+     //   Scanner scannerLocale=new Scanner(System.in);
     //    Date date=new Date();
-    //    System.out.println(date.toString());
 
         Language language = Language.INSTANCE;
         Locale.setDefault(Locale.ENGLISH); // так можно конкретно указать какая locale
-
-        if (args.length==2) {
-        language.setLocale(new Locale(args[0],args[1]));
-
+        Scanner scannerLocale=new Scanner(System.in);
+        Date dateLocale=new Date();
+        String country="";
+        String chooseLanguage="";
+        switch (scannerLocale.next()) {
+            case "1":
+                country = "US";
+                chooseLanguage = "en";
+                break;
+            case "2":
+                country = "RU";
+                chooseLanguage = "ru";
+                break;
+            case "3":
+                country = "BY";
+                chooseLanguage = "be";
+                break;
+            default:
+                if (args.length == 2) {
+                    language.setLocale(new Locale(args[0], args[1]));
+                }
         }
-    //    Locale locale=new Locale("ru", "RU");// создали locale язык русский страна Россия
+        Locale current=new Locale(chooseLanguage,country);
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL, Locale.forLanguageTag(country));
+/*
+        Locale en = new Locale("en","US");
+        DateFormat sdEn = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL, en);
+        System.out.println(sdEn.format(new Date()));
+  */
 
         System.out.println(language.get(Messages.WELCOME));
         System.out.println(language.get(Messages.QUESTION));
         System.out.println(language.get(User.FIRST_NAME));
         System.out.println(language.get(User.LAST_NAME));
-        System.out.println(sdRu.format(new Date()));
+        System.out.println(dateFormat.format(dateLocale));
 
     }
 
