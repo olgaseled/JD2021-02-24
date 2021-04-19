70d64fe8542e39e0085ad3570c56083f164b3ab1
crash-noobik <79578181+crash-noobik@users.noreply.github.com>
2021-04-19 21:57:36 +0300

    JD02-05 Homework

---
+++ src/by/it/levchuk/levchuk/jd02_06/Logger.java
@@ -0,0 +1,20 @@
+package by.it.levchuk.levchuk.jd02_06;
+
+/*crash-noobik
+Liauchuk Aliaksandr*/
+
+public class Logger {
+//    private static volatile Logger logger;
+//    private  Logger(){
+//    }
+//
+//    static Logger get(){
+//        Logger localLogger=logger;
+//        if (localLogger==null){
+//            synchronized (Logger.class){
+//                localLogger=logger;
+//                if (localLogger==null)
+//            }
+//        }
+//    }
+}
