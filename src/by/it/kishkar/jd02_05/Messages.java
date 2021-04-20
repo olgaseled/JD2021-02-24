a6fcaad8ff8850ca737821799003542412dae475
AK1ND <akind1999@mail.ru>
2021-04-20 18:44:50 +0300

    Кишкар jd02_05

---
+++ src/by/it/kishkar/jd02_05/Messages.java
@@ -0,0 +1,7 @@
+package by.it.kishkar.jd02_05;
+
+public interface Messages {
+
+    String WELCOME="messages.welcome";
+    String QUESTION="messages.question";
+}
---
+++ src/by/it/kishkar/jd02_05/txt/converter.bat
@@ -0,0 +1,4 @@
+set path=C:\Games\IntelliJ IDEA Community Edition 2020.3.2\jdk8\bin
+native2ascii -encoding UTF-8 language_en_US.txt ..\resources\language_en_US.properties
+native2ascii -encoding UTF-8 language_be_BY.txt ..\resources\language_be_BY.properties
+native2ascii -encoding UTF-8 language_ru_RU.txt ..\resources\language_ru_RU.properties
