926cd5a4c9f0b455fc5d120b6b536fdcdf00af00
MarinaKhrolovich <maniuneshka@gmail.com>
2021-04-20 22:31:11 +0300

    jd02_06 TaskA

--- src/by/it/khrolovich/jd02_06/Runner.java
+++ src/by/it/khrolovich/jd02_06/Runner.java
@@ -2,7 +2,12 @@ package by.it.khrolovich.jd02_06;
 
 public class Runner {
     public static void main(String[] args) {
-        Logger logger = Logger.get();
-        logger.log("Test");
+        for (int i = 0; i < 20; i++) {
+            new Thread(()->{
+                Logger.get().log("Test");
+            }).start();
+
+        }
+
     }
 }
