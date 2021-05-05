3a75e5c07132e2551860c3c2ccd72be40c1ff701
seledtsova <olgaseledtsova9@yandex.by>
2021-05-05 12:25:19 +0300

    jg01_01

--- src/by/it/seledtsova2/jd01_02/TaskA.java
+++ src/by/it/seledtsova2/jd01_02/TaskA.java
@@ -11,30 +11,45 @@ public class TaskA {
         for (int i = 0; i < array.length; i++) {
             array[i] = scanner.nextInt();
         }
- //       for (int i = 0; i < array.length; i++) {
-  //          System.out.print(array[i] + " ");
-  //      }
         step1(array);
+        step2(array);
     }
 
-
-
-    // 1 2 3 4 55 6 7 8 -9 0
+    // 1 2 3 4 5 -1 -2 -3 -4 -5
     private static void step1(int[] array) {
         int min = array[0];
-        int max=array[0];
+        int max = array[0];
         for (int i = 0; i < array.length; i++) {
             if (min > array[i]) {
                 min = array[i];
             }
-
             if (max < array[i]) {
-                max= array[i];
-
+                max = array[i];
             }
+        }
+        System.out.println(min + " " + max);
+    }
+
+    /*
+    Вывести на консоль те числа, значение которых строго меньше среднегоарифметического(типа double) в этом массиве.
+     */
+
+    private static void step2(int[] array) {
+        double sum = 0;
+        double average;
+        for (int element : array) {
+            sum = sum + element;
+        }
+        average = sum / array.length;
+        System.out.println(sum);
+        System.out.println(average);
+        for (int i = 0; i < array.length; i++) {
+            if (average > array[i]) {
+                System.out.println(array[i]);
+            }
+        }
+
 
         }
-        System.out.println(min+" "+max);
-
     }
-}
+
