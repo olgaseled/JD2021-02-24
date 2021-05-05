97dc1b35a96954dfdd2ac252434751584bb2748f
seledtsova <olgaseledtsova9@yandex.by>
2021-05-05 13:54:02 +0300

    jg01_01

--- src/by/it/seledtsova2/jd01_02/TaskA.java
+++ src/by/it/seledtsova2/jd01_02/TaskA.java
@@ -1,18 +1,25 @@
 package by.it.seledtsova2.jd01_02;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TaskA {
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] array = new int[10];
+ //       int[] array = {11, 12, 11, 13, 14, 15, 16, 16, 8, 9};
+
+//     /*
+    Scanner scanner = new Scanner(System.in);
+       int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
-            array[i] = scanner.nextInt();
+          array[i] = scanner.nextInt();
         }
+
+ //     */
         step1(array);
         step2(array);
+        step3(array);
     }
 
     // 1 2 3 4 5 -1 -2 -3 -4 -5
@@ -28,8 +35,8 @@ public class TaskA {
             }
         }
         System.out.println(min + " " + max);
+        System.out.println();
     }
-
     /*
     Вывести на консоль те числа, значение которых строго меньше среднегоарифметического(типа double) в этом массиве.
      */
@@ -48,8 +55,28 @@ public class TaskA {
                 System.out.println(array[i]);
             }
         }
-
-
-        }
+        System.out.println();
     }
+
+    /*
+    Найти и вывести номер (т.е. индекс) самого маленького числа. Если же таких чисел будет несколько,
+    вывести все найденные индексы минимума через пробел в порядке убывания.
+     */
+    private static void step3(int[] array) {
+        int min = array[0];
+        for (int element: array) {
+            if (element<min) {
+                min=element;
+            }
+        }
+        for (int i = array.length - 1; i >= 0; i--) { // обратный порядок
+                if (array[i] == min) {
+                    System.out.print(i + " ");
+                    }
+            }
+        System.out.println();
+    }
+}
+
+
 
